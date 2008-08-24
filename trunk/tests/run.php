92d3e9cee88d869a393ed85a9398bb9fb74692aa
nycto <nycto@e0e46c49-be69-4f5a-ad62-21024a331aea>
2008-08-24 02:25:58 +0000

    Added error when unit test file doesn't exist
    git-svn-id: http://svn.assembla.com/svn/commonPHP@5 e0e46c49-be69-4f5a-ad62-21024a331aea

--- trunk/tests/run.php
+++ trunk/tests/run.php
@@ -4,26 +4,31 @@
  * Given a file name, this will try to find and run it's unit test
  */
 
-$file = $_SERVER['argv'][1];
+$test = $_SERVER['argv'][1];
 
-if ( $file == __FILE__ )
+if ( $test == __FILE__ )
     die("This file can not be run as a unit test");
 
-$file = preg_replace('/\.php$/i', '', $file);
-$file = explode("/", $file);
+$test = preg_replace('/\.php$/i', '', $test);
+$test = explode("/", $test);
 
-$cutoff = array_search( "src", $file );
+$cutoff = array_search( "src", $test );
 
 if ( $cutoff === FALSE ) {
     
-    $cutoff = array_search( "tests", $file );
+    $cutoff = array_search( "tests", $test );
     
     if ( $cutoff === FALSE )
-        die ( "Could not locate 'src' or 'tests' directory" );
+        die ( "Could not locate 'src' or 'tests' directory in given file: ". $$_SERVER['argv'][1] );
 }
 
-$file = array_slice($file, $cutoff + 1);
+$test = array_slice($test, $cutoff + 1);
 
-system( "phpunit ". implode("_", $file) ." ". __DIR__ ."/". implode("/", $file) .".php" );
+$file = __DIR__ ."/". implode("/", $test) .".php";
+
+if ( !is_file($file) )
+    die("Could not locate unit test file: ". $file);
+
+system( "phpunit ". implode("_", $test) ." ". $file );
 
 ?>
