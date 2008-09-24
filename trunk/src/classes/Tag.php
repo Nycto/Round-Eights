7d99eece309e6c88abd8364b6bf8d0cf06cc7001
nycto <nycto@e0e46c49-be69-4f5a-ad62-21024a331aea>
2008-09-24 03:46:04 +0000

    Continued Tag Class Dev
    git-svn-id: http://svn.assembla.com/svn/commonPHP@33 e0e46c49-be69-4f5a-ad62-21024a331aea

--- trunk/src/classes/Tag.php
+++ trunk/src/classes/Tag.php
@@ -38,6 +38,23 @@ class Tag
     protected $content;
     
     /**
+     * Takes an HTML attribute and strips it down
+     *
+     * @throws cPHP::Exception::Data::Argument Thrown when the attribute name is empty
+     * @param String $attr The name of the attribute
+     * @return String The normalized version of the attribute name
+     */
+    static public function normalizeAttrName ( $attr )
+    {
+        $attr = strtolower( ::cPHP::stripW($attr) );
+        
+        if ( empty($attr) )
+            throw new ::cPHP::Exception::Data::Argument( 0, "Attribute Name", "Must not be empty" );
+        
+        return $attr;
+    }
+    
+    /**
      * Constructor...
      *
      * @param String $tag The tag contained in this instance
@@ -59,9 +76,9 @@ class Tag
     }
     
     /**
-     * Sets the value of the tag in this instace
+     * Sets the value of the tag in this instance
      *
-     * @param String
+     * @param String $tag The tag this instance represents
      * @return object Returns a self reference
      */
     public function setTag ( $tag )
@@ -96,6 +113,162 @@ class Tag
     {
         $content = ::cPHP::strval($content);
         $this->content = empty($content) && $content !== "0" ? null : $content;
+        return $this;
+    }
+    
+    /**
+     * Adds content to the end of the existing content
+     *
+     * @param string $content
+     * @return object Returns a self reference
+     */
+    public function appendContent ( $content )
+    {
+        $content = ::cPHP::strval($content);
+        if ( !empty($content) || $content === "0" )
+            $this->content .= $content;
+        return $this;
+    }
+    
+    /**
+     * Returns whether the current instance has any content
+     *
+     * @return Boolean
+     */
+    public function hasContent ()
+    {
+        return isset($this->content);
+    }
+    
+    /**
+     * Unsets any content in this instance
+     *
+     * @return object Returns a self reference
+     */
+    public function clearContent ()
+    {
+        $this->content = null;
+        return $this;
+    }
+    
+    /**
+     * Sets whether this instance should be rendered as empty
+     *
+     * @param Boolean $setting
+     * @return object Returns a self reference
+     */
+    public function setEmpty ( $setting )
+    {
+        $this->empty = $setting ? TRUE : FALSE;
+        return $this;
+    }
+    
+    /**
+     * Clears the empty override and reverts to automatic detection
+     *
+     * @return object Returns a self reference
+     */
+    public function clearEmpty ()
+    {
+        $this->empty = null;
+        return $this;
+    }
+    
+    /**
+     * Returns whether the current tag will be rendered as empty
+     *
+     * @return Boolean
+     */
+    public function isEmpty ()
+    {
+        // If the have defined an override, return it
+        if ( isset($this->empty) )
+            return $this->empty;
+
+        // If there is content, we will never render as empty
+        else if ( $this->hasContent() )
+            return FALSE;
+
+        // Otherwise, determine it by the type of tag being represented
+        return in_array(
+                $this->tag,
+                array('area', 'base', 'br', 'frame', 'hr', 'img', 'input', 'link', 'meta', 'param')
+            );
+
+    }
+    
+    /**
+     * Returns the list of attributes associated with this instance
+     *
+     * @return Array
+     */
+    public function getAttrs ()
+    {
+        return $this->attrs;
+    }
+    
+    /**
+     * Sets a specific HTML attribute
+     *
+     * @param String $attr The attribute being set
+     * @param object Returns a self reference
+     */
+    public function setAttr ( $attr, $value )
+    {
+        $this->attrs[ self::normalizeAttrName( $attr ) ] = ::cPHP::reduce($value);
+        return $this;
+    }
+    
+    /**
+     * Returns whether an attribute has been set
+     *
+     * @param String $attr The attribute to test
+     * @return Boolean
+     */
+    public function isAttrSet ( $attr )
+    {
+        return array_key_exists( self::normalizeAttrName( $attr ), $this->attrs );
+    }
+    
+    /**
+     * Clears an attribute from being set
+     *
+     * @param String $attr The attribute to test
+     * @param object Returns a self reference
+     */
+    public function clearAttr ( $attr )
+    {
+        $attr = self::normalizeAttrName( $attr );
+        unset( $this->attrs[ $attr ] );
+        return $this;
+    }
+    
+    /**
+     * Sets a specific HTML attribute
+     *
+     * @param String $attr The attribute to fetch
+     * @param mixed Returns the value of the attribute. Returns null if it isn't set
+     */
+    public function getAttr ( $attr )
+    {
+        if ( !$this->isAttrSet($attr) )
+            return null;
+        
+        return $this->attrs[ self::normalizeAttrName( $attr ) ];
+    }
+    
+    /**
+     * Imports a list of attributes in to this instance
+     *
+     * @param mixed $attrs The list of attributes to import
+     * @return object Returns a self reference
+     */
+    public function importAttrs ( $attrs )
+    {
+        $attrs = ::cPHP::Ary::create( $attrs );
+        foreach ( $attrs AS $key => $value ) {
+            $this->setAttr( $key, $value );
+        }
         return $this;
     }
     
--- trunk/tests/classes/Tag.php
+++ trunk/tests/classes/Tag.php
@@ -83,6 +83,9 @@ class classes_tag_tests extends PHPUnit_Framework_TestCase
         $this->assertSame( $tag, $tag->setContent("") );
         $this->assertNull( $tag->getContent() );
         
+        $this->assertSame( $tag, $tag->setContent("   ") );
+        $this->assertEquals( "   ", $tag->getContent() );
+        
         $this->assertSame( $tag, $tag->setContent(FALSE) );
         $this->assertNull( $tag->getContent() );
         
@@ -99,6 +102,250 @@ class classes_tag_tests extends PHPUnit_Framework_TestCase
         
         $this->assertSame( $tag, $tag->setContent($mock) );
         $this->assertSame( "stringified", $tag->getContent() );
+    }
+    
+    public function testAppendContent ()
+    {
+        $tag = new cPHP::Tag("a");
+        
+        $this->assertNull( $tag->getContent() );
+        
+        $this->assertSame( $tag, $tag->appendContent("string") );
+        $this->assertEquals( "string", $tag->getContent() );
+        
+        $this->assertSame( $tag, $tag->appendContent("0") );
+        $this->assertEquals( "string0", $tag->getContent() );
+        
+        $this->assertSame( $tag, $tag->appendContent("") );
+        $this->assertEquals( "string0", $tag->getContent() );
+        
+        $this->assertSame( $tag, $tag->appendContent("  ") );
+        $this->assertEquals( "string0  ", $tag->getContent() );
+        
+        $this->assertSame( $tag, $tag->appendContent(FALSE) );
+        $this->assertEquals( "string0  ", $tag->getContent() );
+        
+        $this->assertSame( $tag, $tag->appendContent(TRUE) );
+        $this->assertEquals( "string0  1", $tag->getContent() );
+        
+        $this->assertSame( $tag, $tag->appendContent(1) );
+        $this->assertEquals( "string0  11", $tag->getContent() );
+        
+        $mock = $this->getMock("stub_getContent", array("__toString"));
+        $mock->expects( $this->once() )
+            ->method("__toString")
+            ->will( $this->returnValue("stringified"));
+        
+        $this->assertSame( $tag, $tag->appendContent($mock) );
+        $this->assertEquals( "string0  11stringified", $tag->getContent() );
+    }
+    
+    public function testClearContent ()
+    {
+        $tag = new cPHP::Tag("a");
+        
+        $this->assertNull( $tag->getContent() );
+        
+        $tag->setContent("This is a string") ;
+        $this->assertEquals( "This is a string", $tag->getContent() );
+        
+        $this->assertSame( $tag, $tag->clearContent() );
+        
+        $this->assertNull( $tag->getContent() );
+    }
+    
+    public function testHasContent ()
+    {
+        $tag = new cPHP::Tag("a");
+        
+        $this->assertFalse( $tag->hasContent() );
+        
+        $tag->setContent("This is a string") ;
+        
+        $this->assertTrue( $tag->hasContent() );
+        
+        $tag->clearContent();
+        
+        $this->assertFalse( $tag->hasContent() );
+    }
+    
+    public function testIsEmpty ()
+    {
+        $tag = new cPHP::Tag("hr");
+        $this->assertTrue( $tag->isEmpty() );
+        
+        $tag->setContent("Random");
+        $this->assertFalse( $tag->isEmpty() );
+        
+        $tag->clearContent();
+        $this->assertTrue( $tag->isEmpty() );
+
+        $tag->setTag('a');
+        $this->assertFalse( $tag->isEmpty() );
+    }
+    
+    public function testSetEmpty ()
+    {
+        $tag = new cPHP::Tag("hr");
+        $this->assertTrue( $tag->isEmpty() );
+        
+        $this->assertSame( $tag, $tag->setEmpty(FALSE) );
+        $this->assertFalse( $tag->isEmpty() );
+        
+        $this->assertSame( $tag, $tag->setEmpty(TRUE) );
+        $this->assertTrue( $tag->isEmpty() );
+        
+        
+        $tag = new cPHP::Tag("a");
+        $this->assertFalse( $tag->isEmpty() );
+        
+        $this->assertSame( $tag, $tag->setEmpty(TRUE) );
+        $this->assertTrue( $tag->isEmpty() );
+        
+        $this->assertSame( $tag, $tag->setEmpty(FALSE) );
+        $this->assertFalse( $tag->isEmpty() );
+    }
+    
+    public function testClearEmpty ()
+    {
+        $tag = new cPHP::Tag("hr");
+        $this->assertTrue( $tag->isEmpty() );
+        
+        $this->assertSame( $tag, $tag->setEmpty(FALSE) );
+        $this->assertFalse( $tag->isEmpty() );
+        
+        $this->assertSame( $tag, $tag->clearEmpty() );
+        $this->assertTrue( $tag->isEmpty() );
+        
+        
+        $tag = new cPHP::Tag("a");
+        $this->assertFalse( $tag->isEmpty() );
+        
+        $this->assertSame( $tag, $tag->setEmpty(TRUE) );
+        $this->assertTrue( $tag->isEmpty() );
+        
+        $this->assertSame( $tag, $tag->clearEmpty() );
+        $this->assertFalse( $tag->isEmpty() );
+    }
+    
+    public function testNormalizeAttrName ()
+    {
+        $this->assertSame( "attr", ::cPHP::Tag::normalizeAttrName("Attr") );
+        $this->assertSame( "attr", ::cPHP::Tag::normalizeAttrName(" a!@#t tr ") );
+        
+        try {
+            ::cPHP::Tag::normalizeAttrName("  ");
+            $this->fail("An expected exception has not been thrown");
+        }
+        catch ( cPHP::Exception::Data::Argument $err ) {
+            $this->assertEquals( "Must not be empty", $err->getMessage() );
+        }
+    }
+    
+    public function testSetAttr ()
+    {
+        $tag = new cPHP::Tag("hr");
+        
+        $this->assertSame( $tag, $tag->setAttr("Rel", "nofollow") );
+        $this->assertSame( array("rel" => "nofollow"), $tag->getAttrs() );
+        
+        $this->assertSame( $tag, $tag->setAttr("class", "title") );
+        $this->assertSame( array("rel" => "nofollow", "class" => "title"), $tag->getAttrs() );
+        
+        $this->assertSame( $tag, $tag->setAttr("class", "link") );
+        $this->assertSame( array("rel" => "nofollow", "class" => "link"), $tag->getAttrs() );
+        
+        try {
+            $tag->setAttr("  ", "empty");
+            $this->fail("An expected exception has not been thrown");
+        }
+        catch ( cPHP::Exception::Data::Argument $err ) {
+            $this->assertEquals( "Must not be empty", $err->getMessage() );
+        }
+    }
+    
+    public function testIsAttrSet ()
+    {
+        $tag = new cPHP::Tag("hr");
+        
+        $this->assertFalse( $tag->isAttrSet("class") );
+        
+        $tag->setAttr("class", "title");
+        
+        $this->assertTrue( $tag->isAttrSet("class") );
+        
+        try {
+            $tag->isAttrSet("  ", "empty");
+            $this->fail("An expected exception has not been thrown");
+        }
+        catch ( cPHP::Exception::Data::Argument $err ) {
+            $this->assertEquals( "Must not be empty", $err->getMessage() );
+        }
+    }
+    
+    public function testClearAttr ()
+    {
+        $tag = new cPHP::Tag("hr");
+        
+        $this->assertSame( $tag, $tag->clearAttr("rel") );
+        
+        $tag->setAttr("rel", "nofollow")
+            ->setAttr("class", "title");
+        
+        $this->assertSame( array("rel" => "nofollow", "class" => "title"), $tag->getAttrs() );
+        
+        $this->assertSame( $tag, $tag->clearAttr("rel") );
+        
+        $this->assertSame( array("class" => "title"), $tag->getAttrs() );
+        
+        try {
+            $tag->clearAttr("  ");
+            $this->fail("An expected exception has not been thrown");
+        }
+        catch ( cPHP::Exception::Data::Argument $err ) {
+            $this->assertEquals( "Must not be empty", $err->getMessage() );
+        }
+    }
+    
+    public function testGetAttr ()
+    {
+        $tag = new cPHP::Tag("hr");
+        
+        $this->assertNull( $tag->getAttr("rel") );
+        
+        $tag->setAttr("rel", "nofollow");
+        
+        $this->assertSame( "nofollow", $tag->getAttr("rel") );
+        
+        try {
+            $tag->getAttr("  ");
+            $this->fail("An expected exception has not been thrown");
+        }
+        catch ( cPHP::Exception::Data::Argument $err ) {
+            $this->assertEquals( "Must not be empty", $err->getMessage() );
+        }
+    }
+    
+    public function testImportAttrs ()
+    {
+        $tag = new cPHP::Tag("hr");
+        
+        $this->assertSame( array(), $tag->getAttrs() );
+        
+        $this->assertSame(
+                $tag,
+                $tag->importAttrs( array( "rel" => "nofollow", "class" => "link" ) )
+            );
+        
+        $this->assertSame( array( "rel" => "nofollow", "class" => "link" ), $tag->getAttrs() );
+        
+        try {
+            $tag->importAttrs( array( "rel" => "nofollow", "  " => "link" ) );
+            $this->fail("An expected exception has not been thrown");
+        }
+        catch ( cPHP::Exception::Data::Argument $err ) {
+            $this->assertEquals( "Must not be empty", $err->getMessage() );
+        }
     }
     
 }
