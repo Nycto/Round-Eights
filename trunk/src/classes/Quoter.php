327362b7ac83a2f6bcc10e64116e9e691f8b1068
nycto <nycto@e0e46c49-be69-4f5a-ad62-21024a331aea>
2008-09-28 03:27:57 +0000

    Further quoter development
    git-svn-id: http://svn.assembla.com/svn/commonPHP@41 e0e46c49-be69-4f5a-ad62-21024a331aea

--- trunk/src/classes/Quoter.php
+++ trunk/src/classes/Quoter.php
@@ -14,7 +14,7 @@ class Quoter
 {
     
     /**
-     * The escape character in the string
+     * The escape string
      */
     protected $escape = "\\";
 
@@ -29,6 +29,107 @@ class Quoter
             "'" => array( "'" ),
             '"' => array( '"' )
         );
+    
+    /**
+     * Returns whether an offset is preceded by an unescaped escape string
+     *
+     * @param String $string The haystack
+     * @param Integer $offset The offset to walk back from
+     * @param String $escape The escape character
+     * @return Boolean
+     */
+    static public function isEscaped ( $string, $offset, $escape = '\\' )
+    {
+        $escape = ::cPHP::strval( $escape );
+        
+        // Something can't be escaped if there is no escape string
+        if ( ::cPHP::is_empty( $escape, ALLOW_SPACES ) )
+            return false;
+        
+        $string = ::cPHP::strval( $string );
+        $offset = intval( $offset );
+        
+        if ( $offset > strlen( $string ) )
+            return false;
+        
+        $escapeLen = strlen( $escape );
+        
+        // If there isn't enough room for the escape string, how could it be escaped?
+        if ( $offset - $escapeLen < 0 )
+            return false;
+        
+        // If the preceding characters are not the escape string, then it is definitely not escaped
+        if ( strcasecmp( substr($string, $offset - $escapeLen, $escapeLen), $escape ) != 0 )
+            return false;
+        
+        // Now, we need to determine whether the escape string is escaped
+        return !self::isEscaped( $string, $offset - $escapeLen, $escape );
+    }
+    
+    /**
+     * Returns the position of the next unescaped character in a string
+     *
+     * @param String $string The haystack
+     * @param Array $needles The list of strings to look for
+     * @param String $escape The escape character to use
+     */
+    static public function findNext ( $string, array $needles, $escape = '\\' )
+    {
+        if ( count($needles) <= 0 )
+            return array( false, false );
+        
+        $resultOffset = false;
+        $resultNeedle = false;
+        
+        // Loop through each needle so we can figure out the one with the minimum offset
+        foreach( $needles AS $needle ) {
+            
+            $needle = ::cPHP::strval( $needle );
+            
+            if ( ::cPHP::is_empty( $needle, ALLOW_SPACES ) )
+                throw new ::cPHP::Exception::Data($needle, "needle", "Needle must not be empty");
+            
+            // Cache the length of the needle so it isn't continually calculated
+            $needleLen = strlen( $needle );
+            
+            // Give $pos an initial value because it is used by
+            // stripos to determine where to start looking
+            $pos = 0 - $needleLen;
+            
+            do {
+                
+                $pos = stripos(
+                        $string,
+                        $needle,
+                        
+                        // Since $pos represents the location of the last found needle,
+                        // to avoid an infinite loops, we need to start search at the
+                        // offset just after the last needle was found
+                        $pos + $needleLen
+                    );
+                
+                // If $pos is false, it means that the needle was not found in the string at all
+                // In this circumstance, we can immediately break out of this loop and
+                // continue with the next needle. Hence "continue 2"
+                if ( $pos === FALSE )
+                    continue 2;
+                
+            // Continue searching if this character is escaped
+            } while ( self::isEscaped($string, $pos, $escape) );
+            
+            // If we have not yet found any needles, or this needle appears
+            // before any of the other ones we've found so far, then mark
+            // it as the one that will be returned
+            if ( $resultOffset === FALSE || $pos < $resultOffset ) {
+                $resultOffset = $pos;
+                $resultNeedle = $needle;
+            }
+            
+        }
+        
+        return array( $resultOffset, $resultNeedle );
+        
+    }
     
     /**
      * Returns the list of quotes registered in this instance
@@ -79,10 +180,13 @@ class Quoter
         }
         else {
             
-            $close = new ::cPHP::Ary( $close );
-            $close->flatten()->collect("::cPHP::strval")->compact( ALLOW_SPACES )->unique();
+            $close = ::cPHP::Ary::create( $close )
+                ->flatten()
+                ->collect("cPHP::strval")
+                ->compact( ALLOW_SPACES )
+                ->unique()
+                ->get();
             
-            var_dump( $close->get() );
         }
         
         $this->quotes[ $open ] = $close;
@@ -93,11 +197,115 @@ class Quoter
     /**
      * Returns a flat list of all the open and close quotes registered in this instance
      *
-     * @return array
+     * @return Object Returns a cPHP::Ary object
      */
     public function getAllQuotes ()
     {
+        return cPHP::Ary::create( array_values( $this->quotes ) )
+            ->flatten()
+            ->merge( array_keys( $this->quotes ) )
+            ->unique()
+            ->values();
+    }
+    
+    /**
+     * Returns a list of all the opening quotes
+     *
+     * @return Object Returns a cPHP::Ary object
+     */
+    public function getOpenQuotes ()
+    {
+        return new cPHP::Ary( array_keys( $this->quotes ) );
+    }
+    
+    /**
+     * Returns whether a given quote is an opening quote
+     *
+     * @param String $quote The quote to check
+     * @return Boolean
+     */
+    public function isOpenQuote ( $quote )
+    {
+        $quote = ::cPHP::strval( $quote );
+        return array_key_exists( $quote, $this->quotes );
+    }
+    
+    /**
+     * Returns a list of closing quotes for an opening quote
+     *
+     * @param String $quote The opening quote
+     * @return Object Returns a cPHP::Ary object
+     */
+    public function getCloseQuotesFor ( $quote )
+    {
+        $quote = ::cPHP::strval( $quote );
         
+        if ( !$this->isOpenQuote($quote) )
+            throw new ::cPHP::Exception::Argument( 0, "Open Quote", "Invalid open quote" );
+        
+        return cPHP::Ary::create( $this->quotes[ $quote ] )->values();
+    }
+    
+    /**
+     * Returns the escape string in this instance
+     *
+     * @return Boolean|String Returns the escape string, or null if there isn't one set
+     */
+    public function getEscape ()
+    {
+        if ( !isset($this->escape) )
+            return null;
+        else
+            return $this->escape;
+    }
+    
+    /**
+     * Sets the escape string
+     *
+     * @param String The new escape string
+     * @return Object Returns a self reference
+     */
+    public function setEscape ( $escape )
+    {
+        $escape = ::cPHP::strval( $escape );
+        if ( ::cPHP::is_empty( $escape, ALLOW_SPACES ) )
+            throw new ::cPHP::Exception::Argument( 0, "Escape String", "Must not be empty" );
+        $this->escape = $escape;
+        return $this;
+    }
+    
+    /**
+     * Removes the escape string
+     *
+     * @return Object Returns a self reference
+     */
+    public function clearEscape ()
+    {
+        $this->escape = null;
+        return $this;
+    }
+    
+    /**
+     * Returns whether there is an escape string in this instance
+     *
+     * @return Boolean
+     */
+    public function escapeExists ()
+    {
+        return isset( $this->escape );
+    }
+    
+    /**
+     * Breaks a string up according the settings in this instance
+     *
+     * @param String $string The string to parse
+     * @return Object Returns a ::cPHP::Quoter::Parsed object
+     */
+    public function parse ( $string )
+    {
+        $string = ::cPHP::strval( $string );
+        
+        $openQuotes = $this->getOpenQuotes()->get();
     }
     
 }
--- trunk/tests/classes/Quoter.php
+++ trunk/tests/classes/Quoter.php
@@ -29,14 +29,18 @@ class classes_quoter
 class classes_quoter_tests extends PHPUnit_Framework_TestCase
 {
     
-    public function testInitialQuotes ()
+    public function testInitial ()
     {
         $quoter = new ::cPHP::Quoter;
         
+        $this->assertThat( $quoter->getQuotes(), $this->isInstanceOf("cPHP::Ary") );
+        
         $this->assertSame(
                 array( "'" => array( "'" ), '"' => array( '"' ) ),
-                $quoter->getQuotes()
+                $quoter->getQuotes()->get()
             );
+        
+        $this->assertSame( '\\', $quoter->getEscape() );
     }
     
     public function testClearQuotes ()
@@ -45,14 +49,14 @@ class classes_quoter_tests extends PHPUnit_Framework_TestCase
         
         $this->assertSame(
                 array( "'" => array( "'" ), '"' => array( '"' ) ),
-                $quoter->getQuotes()
+                $quoter->getQuotes()->get()
             );
         
         $this->assertSame( $quoter, $quoter->clearQuotes() );
         
         $this->assertSame(
                 array(),
-                $quoter->getQuotes()
+                $quoter->getQuotes()->get()
             );
     }
     
@@ -63,14 +67,14 @@ class classes_quoter_tests extends PHPUnit_Framework_TestCase
         
         $this->assertSame(
                 array(),
-                $quoter->getQuotes()
+                $quoter->getQuotes()->get()
             );
         
         $this->assertSame( $quoter, $quoter->setQuote( "`" ) );
         
         $this->assertSame(
                 array( "`" => array("`") ),
-                $quoter->getQuotes()
+                $quoter->getQuotes()->get()
             );
         
         
@@ -78,8 +82,274 @@ class classes_quoter_tests extends PHPUnit_Framework_TestCase
         
         $this->assertSame(
                 array( "`" => array("`"), "(" => array( ")" ) ),
-                $quoter->getQuotes()
+                $quoter->getQuotes()->get()
             );
+    }
+    
+    public function testGetAllQuotes ()
+    {
+        $quoter = new ::cPHP::Quoter;
+        $quoter->setQuote( "'", array( "'", '"', '`' ) );
+        $quoter->setQuote( "!", array( '@', '#', '*' ) );
+        
+        $quotes = $quoter->getAllQuotes();
+        $this->assertThat( $quotes, $this->isInstanceOf("cPHP::Ary") );
+        $this->assertEquals(
+                array( "'", '"', '`', '@', '#', '*', "!" ),
+                $quotes->get()
+            );
+    }
+    
+    public function testGetOpenQuotes ()
+    {
+        $quoter = new ::cPHP::Quoter;
+        $quoter->setQuote( "'", array( "'", '"', '`' ) );
+        $quoter->setQuote( "!", array( '@', '#', '*' ) );
+        $quoter->setQuote( "(", ")" );
+        $quoter->setQuote( "`" );
+        
+        $quotes = $quoter->getOpenQuotes();
+        $this->assertThat( $quotes, $this->isInstanceOf("cPHP::Ary") );
+        $this->assertEquals(
+                array( "'", '"', '!', '(', '`' ),
+                $quotes->get()
+            );
+    }
+    
+    public function testIsOpenQuote ()
+    {
+        $quoter = new ::cPHP::Quoter;
+        $quoter->setQuote( "(", ")" );
+        $quoter->setQuote( "`" );
+        
+        $this->assertTrue( $quoter->isOpenQuote("(") );
+        $this->assertTrue( $quoter->isOpenQuote("`") );
+        $this->assertFalse( $quoter->isOpenQuote(")") );
+    }
+    
+    public function testGetCloseQuotesFor ()
+    {
+        $quoter = new ::cPHP::Quoter;
+        $quoter->setQuote( "(", ")" );
+        $quoter->setQuote( "`" );
+        $quoter->setQuote( "!", array( '@', '#', '*' ) );
+        
+        $quotes = $quoter->getCloseQuotesFor( "(" );
+        $this->assertThat( $quotes, $this->isInstanceOf("cPHP::Ary") );
+        $this->assertEquals( array( ')' ), $quotes->get() );
+        
+        $quotes = $quoter->getCloseQuotesFor( "!" );
+        $this->assertThat( $quotes, $this->isInstanceOf("cPHP::Ary") );
+        $this->assertEquals( array( '@', '#', '*' ), $quotes->get() );
+    }
+    
+    public function testSetEscape ()
+    {
+        $quoter = new ::cPHP::Quoter;
+        $this->assertSame( $quoter, $quoter->setEscape( "new" ) );
+        $this->assertSame( "new", $quoter->getEscape() );
+        
+        try {
+            $quoter->setEscape("");
+            $this->fail("An expected exception was not thrown");
+        }
+        catch ( cPHP::Exception::Argument $err ) {
+            $this->assertSame( "Must not be empty", $err->getMessage() );
+        }
+    }
+    
+    public function testClearEscape ()
+    {
+        $quoter = new ::cPHP::Quoter;
+        $quoter->setEscape( '\\' );
+        
+        $this->assertSame( $quoter, $quoter->clearEscape() );
+        $this->assertNull( $quoter->getEscape() );
+    }
+    
+    public function testEscapeExists ()
+    {
+        $quoter = new ::cPHP::Quoter;
+        $quoter->setEscape( '\\' );
+        
+        $this->assertTrue( $quoter->escapeExists() );
+        
+        $quoter->clearEscape();
+        
+        $this->assertFalse( $quoter->escapeExists() );
+    }
+    
+    public function testIsEscaped ()
+    {
+        $this->assertFalse( ::cPHP::Quoter::isEscaped("isn\\'t it escaped?", 90) );
+        
+        $this->assertFalse( ::cPHP::Quoter::isEscaped("isn\\'t it escaped?", 0) );
+        $this->assertFalse( ::cPHP::Quoter::isEscaped("isn\\'t it escaped?", 8) );
+        
+        $this->assertTrue( ::cPHP::Quoter::isEscaped("isn\\'t it escaped?", 4) );
+        $this->assertFalse( ::cPHP::Quoter::isEscaped("isn\\\\'t it escaped?", 5) );
+        $this->assertTrue( ::cPHP::Quoter::isEscaped("isn\\\\\\'t it escaped?", 6) );
+        $this->assertFalse( ::cPHP::Quoter::isEscaped("isn\\\\\\\\'t it escaped?", 7) );
+        $this->assertTrue( ::cPHP::Quoter::isEscaped("isn\\\\\\\\\\'t it escaped?", 8) );
+        $this->assertFalse( ::cPHP::Quoter::isEscaped("isn\\\\\\\\\\'t it escaped?", 9) );
+        $this->assertTrue( ::cPHP::Quoter::isEscaped("isn\\\\\\\\\\\\\\'t it escaped?", 10) );
+        
+        $this->assertFalse( ::cPHP::Quoter::isEscaped("\\\\isn't it escaped?", 2) );
+        
+        
+        $this->assertFalse( ::cPHP::Quoter::isEscaped("isn't it escaped?", 2, '/esc/') );
+        $this->assertTrue( ::cPHP::Quoter::isEscaped("isn/esc/'t it escaped?", 8, '/esc/') );
+        $this->assertTrue( ::cPHP::Quoter::isEscaped("isn/EsC/'t it escaped?", 8, '/esc/') );
+        $this->assertFalse( ::cPHP::Quoter::isEscaped("isn/Esc//EsC/'t it escaped?", 13, '/esc/') );
+        $this->assertTrue( ::cPHP::Quoter::isEscaped("isn/EsC//Esc//EsC/'t it escaped?", 18, '/esc/') );
+        
+        $this->assertTrue( ::cPHP::Quoter::isEscaped("/esc/isn't it escaped?", 5, '/esc/') );
+        
+        $this->assertFalse( ::cPHP::Quoter::isEscaped("isn\\'t it escaped?", 4, null) );
+        $this->assertFalse( ::cPHP::Quoter::isEscaped("isn\\'t it escaped?", 8, null) );
+    }
+    
+    public function testFindNext ()
+    {
+        try {
+            ::cPHP::Quoter::findNext( "string", array ('"', "") );
+            $this->fail("An expected exception was not thrown");
+        }
+        catch ( cPHP::Exception::Data $err ) {
+            $this->assertSame( "Needle must not be empty", $err->getMessage() );
+        }
+        
+        $this->assertSame(
+                array(false, false),
+                ::cPHP::Quoter::findNext( "string", array() )
+            );
+        
+        $this->assertSame(
+                array(8, "'"),
+                ::cPHP::Quoter::findNext(
+                        "It\\'s a 'quoted' string",
+                        array ('"', "'"),
+                        '\\'
+                    )
+            );
+        
+        $this->assertSame(
+                array(false, false),
+                ::cPHP::Quoter::findNext(
+                        "It\\'s a \\'quoted\\'  string",
+                        array ('"', "'"),
+                        '\\'
+                    )
+            );
+        
+        $this->assertSame(
+                array(0, '"'),
+                ::cPHP::Quoter::findNext(
+                        '"Its a \\"quoted\\"  string',
+                        array ('"', "'"),
+                        '\\'
+                    )
+            );
+        
+        $this->assertSame(
+                array(false, false),
+                ::cPHP::Quoter::findNext(
+                        'String without quotes',
+                        array ('"', "'"),
+                        '\\'
+                    )
+            );
+        
+        $this->assertSame(
+                array(false, false),
+                ::cPHP::Quoter::findNext(
+                        'String \"without\" quotes',
+                        array ('"', "'"),
+                        '\\'
+                    )
+            );
+        
+        $this->assertSame(
+                array(6, "'"),
+                ::cPHP::Quoter::findNext(
+                        'String\' "with" \'quotes',
+                        array ('"', "'"),
+                        '\\'
+                    )
+            );
+        
+        $this->assertSame(
+                array(6, '"'),
+                ::cPHP::Quoter::findNext(
+                        "String\" 'with' \"quotes",
+                        array ('"', "'"),
+                        '\\'
+                    )
+            );
+        
+        $this->assertSame(
+                array(19, "'"),
+                ::cPHP::Quoter::findNext(
+                        "String with a quote'",
+                        array ('"', "'"),
+                        '\\'
+                    )
+            );
+        
+        $this->assertSame(
+                array(12, "QT"),
+                ::cPHP::Quoter::findNext(
+                        "String with QTa quote",
+                        array ('QT'),
+                        '\\'
+                    )
+            );
+        
+        $this->assertSame(
+                array(22, "QT"),
+                ::cPHP::Quoter::findNext(
+                        "String with \\QTa quoteQT",
+                        array ('QT'),
+                        '\\'
+                    )
+            );
+        
+        $this->assertSame(
+                array(12, "QT"),
+                ::cPHP::Quoter::findNext(
+                        "String with qtQT2 a quote",
+                        array ('QT', 'QT2'),
+                        '\\'
+                    )
+            );
+        
+        $this->assertSame(
+                array(14, "QT"),
+                ::cPHP::Quoter::findNext(
+                        "String q with qt a quote",
+                        array ('QT', 'QT2'),
+                        '\\'
+                    )
+            );
+        
+        $this->assertSame(
+                array(12, "QT"),
+                ::cPHP::Quoter::findNext(
+                        "String with QT2a quote",
+                        array ('QT', 'QT2'),
+                        '\\'
+                    )
+            );
+        
+        $this->assertSame(
+                array(7, "with"),
+                ::cPHP::Quoter::findNext(
+                        "String with a quote",
+                        array ('with', 'it'),
+                        '\\'
+                    )
+            );
+        
     }
     
 }
