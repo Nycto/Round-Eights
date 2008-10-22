5f2122d23b716d0c8262c11c1a1b426e2c6eae00
nycto <nycto@e0e46c49-be69-4f5a-ad62-21024a331aea>
2008-10-22 02:50:05 +0000

    Querier Development
    git-svn-id: http://svn.assembla.com/svn/commonPHP@98 e0e46c49-be69-4f5a-ad62-21024a331aea

--- branches/dbDev/src/classes/DB/LinkWrap/Querier.php
+++ branches/dbDev/src/classes/DB/LinkWrap/Querier.php
@@ -128,10 +128,99 @@ class Querier extends ::cPHP::DB::LinkWrap
         if ( $result === FALSE )
             return FALSE;
         
-        $id = $result->getInsertID();
-        $result->free();
+        return $result->getInsertID();
+    }
+    
+    /**
+     * Updates a table with the given values
+     *
+     * Like the insert method, the fields parameter should be an associative
+     * array or equivilent object. The keys represent field names and the
+     * array values are the new field values.
+     *
+     * @param String $table The table to update
+     * @param String $where Any WHERE clause restrictions to place on the query
+     * @param Array|Object $fields The associative array of fields update
+     * @param Integer $flags Any query flags to use
+     * @return Object Returns a Result object
+     */
+    public function update ($table, $where, $fields, $flags = 0)
+    {
+        $table = ::cPHP::strval($table);
 
-        return $result;
+        if ( ::cPHP::is_empty($table) )
+            throw new ::cPHP::Exception::Argument(0, "Table Name", "Must not be empty");
+        
+        $query = "UPDATE ". $table ." SET ". $this->getFieldList($fields);
+        
+        $where = trim( ::cPHP::strval($where) );
+        
+        if ( !::cPHP::is_empty($where) )
+            $query .= " WHERE ". $where;
+
+        return $this->query($query, $flags);
+    }
+    
+    /**
+     * Runs a query and returns a specific row
+     *
+     * If no row is specified, the first one will be returned
+     *
+     * @param String $query The query to execute
+     * @param Integer $row The row of a multi-result set to pull the field from
+     * @param Integer $flags Any query flags to use
+     * @return mixed Returns the row of the result, or returns FALSE if no results were returned
+     */
+    public function getRow ($query, $row = 0, $flags = 0)
+    {
+        $result = $this->query($query, $flags);
+
+        if ( !($result instanceof ::cPHP::DB::Result::Read) ) {
+            $err = new ::cPHP::Exception::DB::Query($query, "Query did not a valid Read result object");
+            $err->addData("Returned Result", ::cPHP::getDump($result));
+            throw $err;
+        }
+
+        if ($result->count() <= 0)
+            return FALSE;
+        
+        $value = $result->seek( $row )->current();
+        
+        $result->free();
+        
+        return $value;
+    }
+    
+    /**
+     * Runs a query and returns a specific field from a row
+     *
+     * If no row is specified, the first row will be used
+     *
+     * @param String $field The field to return
+     * @param String $query The query to execute
+     * @param Integer $row The row of a multi-result set to pull the field from
+     * @return mixed Returns the value of the field, or FALSE if no results were returned
+     */
+    public function getField ($field, $query, $row = 0, $flags = 0)
+    {
+        $result = $this->query($query, $flags);
+
+        if (!$result instanceof cDB_result)
+            throw new QueryError($query, "Query did not return a valid result set");
+
+        if ($result->get_num_rows() <= 0) {
+            return FALSE;
+        }
+        else {
+            $field = $result->isField($field);
+            if (!$field)
+                throw new ArgumentError(0, "field name", "Field does not exist in this result set");
+
+            $value = $result->seek( $row );
+            $result->free();
+            return $value[$field];
+        }
+
     }
     
 }
--- branches/dbDev/tests/classes/DB/LinkWrap/Querier.php
+++ branches/dbDev/tests/classes/DB/LinkWrap/Querier.php
@@ -177,15 +177,15 @@ class classes_db_linkwrap_querier extends PHPUnit_Framework_TestCase
         
     }
 
-    public function testInsert ()
+    public function testInsert_Errors ()
     {
         
-        $Link = $this->getMock(
+        $link = $this->getMock(
                 "cPHP::iface::DB::Link",
                 array("query", "quote", "escape")
             );
         
-        $query = new ::cPHP::DB::LinkWrap::Querier( $Link );
+        $query = new ::cPHP::DB::LinkWrap::Querier( $link );
         
         try {
             $query->insert( "", array() );
@@ -210,8 +210,314 @@ class classes_db_linkwrap_querier extends PHPUnit_Framework_TestCase
         catch ( ::cPHP::Exception::Argument $err ) {
             $this->assertSame("Must be an array or traversable", $err->getMessage());
         }
+    
+    }
+    
+    public function testInsert_Success ()
+    {
         
-        $this->markTestIncomplete("Results need to be implemented before this can be completed");
+        $link = $this->getMock(
+                "cPHP::iface::DB::Link",
+                array("query", "quote", "escape")
+            );
+        
+        $query = new ::cPHP::DB::LinkWrap::Querier( $link );
+        
+        $link->expects($this->once())
+            ->method("query")
+            ->with( $this->equalTo("INSERT INTO table SET `field1` = 404, `field2` = 'error'" ) )
+            ->will( $this->returnValue(
+                    new ::cPHP::DB::Result::Write(1, 20, "INSERT")
+                ));
+            
+        $link->expects($this->at(0))
+            ->method("quote")
+            ->with( $this->equalTo(404) )
+            ->will( $this->returnValue( 404 ));
+            
+        $link->expects($this->at(1))
+            ->method("quote")
+            ->with( $this->equalTo('error') )
+            ->will( $this->returnValue("'error'"));
+        
+        $this->assertSame(
+                20,
+                $query->insert("table", array('field1' => 404, 'field2' => 'error'))
+            );
+    }
+    
+    public function testInsert_ReturnFalse ()
+    {
+        
+        $link = $this->getMock(
+                "cPHP::iface::DB::Link",
+                array("query", "quote", "escape")
+            );
+        
+        $query = new ::cPHP::DB::LinkWrap::Querier( $link );
+        
+        $link->expects($this->once())
+            ->method("query")
+            ->with( $this->equalTo("INSERT INTO table SET `field1` = 404, `field2` = 'error'" ) )
+            ->will( $this->returnValue( FALSE ));
+            
+        $link->expects($this->at(0))
+            ->method("quote")
+            ->with( $this->equalTo(404) )
+            ->will( $this->returnValue( 404 ));
+            
+        $link->expects($this->at(1))
+            ->method("quote")
+            ->with( $this->equalTo('error') )
+            ->will( $this->returnValue("'error'"));
+            
+        $this->assertFalse(
+                $query->insert("table", array('field1' => 404, 'field2' => 'error'))
+            );
+    }
+    
+    public function testUpdate_Errors ()
+    {
+        
+        $link = $this->getMock(
+                "cPHP::iface::DB::Link",
+                array("query", "quote", "escape")
+            );
+        
+        $query = new ::cPHP::DB::LinkWrap::Querier( $link );
+        
+        try {
+            $query->update( "", null, array() );
+            $this->fail("An expected exception was not thrown");
+        }
+        catch ( ::cPHP::Exception::Argument $err ) {
+            $this->assertSame("Must not be empty", $err->getMessage());
+        }
+        
+        try {
+            $query->update( "tablename", null, array() );
+            $this->fail("An expected exception was not thrown");
+        }
+        catch ( ::cPHP::Exception::Argument $err ) {
+            $this->assertSame("Must not be empty", $err->getMessage());
+        }
+        
+        try {
+            $query->update( "tablename", null, "some string" );
+            $this->fail("An expected exception was not thrown");
+        }
+        catch ( ::cPHP::Exception::Argument $err ) {
+            $this->assertSame("Must be an array or traversable", $err->getMessage());
+        }
+    
+    }
+    
+    public function testUpdate_NoWhere ()
+    {
+        
+        $link = $this->getMock(
+                "cPHP::iface::DB::Link",
+                array("query", "quote", "escape")
+            );
+        
+        $query = new ::cPHP::DB::LinkWrap::Querier( $link );
+        
+        $result = new ::cPHP::DB::Result::Write(1, null, "UPDATE");
+        
+        $link->expects($this->once())
+            ->method("query")
+            ->with( $this->equalTo("UPDATE table SET `field1` = 404, `field2` = 'error'" ) )
+            ->will( $this->returnValue( $result ));
+            
+        $link->expects($this->at(0))
+            ->method("quote")
+            ->with( $this->equalTo(404) )
+            ->will( $this->returnValue( 404 ));
+            
+        $link->expects($this->at(1))
+            ->method("quote")
+            ->with( $this->equalTo('error') )
+            ->will( $this->returnValue("'error'"));
+        
+        $this->assertSame(
+                $result,
+                $query->update("table", null, array('field1' => 404, 'field2' => 'error'))
+            );
+    }
+    
+    public function testUpdate_WithWhere ()
+    {
+        
+        $link = $this->getMock(
+                "cPHP::iface::DB::Link",
+                array("query", "quote", "escape")
+            );
+        
+        $query = new ::cPHP::DB::LinkWrap::Querier( $link );
+        
+        $result = new ::cPHP::DB::Result::Write(1, null, "UPDATE");
+        
+        $link->expects($this->once())
+            ->method("query")
+            ->with( $this->equalTo("UPDATE table SET `field1` = 404, `field2` = 'error' WHERE id > 5" ) )
+            ->will( $this->returnValue( $result ));
+            
+        $link->expects($this->at(0))
+            ->method("quote")
+            ->with( $this->equalTo(404) )
+            ->will( $this->returnValue( 404 ));
+            
+        $link->expects($this->at(1))
+            ->method("quote")
+            ->with( $this->equalTo('error') )
+            ->will( $this->returnValue("'error'"));
+        
+        $this->assertSame(
+                $result,
+                $query->update("table", "id > 5", array('field1' => 404, 'field2' => 'error'))
+            );
+    }
+    
+    public function testGetRow_WrongResult ()
+    {
+        
+        $link = $this->getMock(
+                "cPHP::iface::DB::Link",
+                array("query", "quote", "escape")
+            );
+        
+        $query = new ::cPHP::DB::LinkWrap::Querier( $link );
+        
+        
+        $link->expects($this->once())
+            ->method("query")
+            ->with( $this->equalTo("UPDATE table SET id = 1" ) )
+            ->will( $this->returnValue( new cPHP::DB::Result::Write(0, null, "UPDATE") ));
+    
+        try {
+            $query->getRow( "UPDATE table SET id = 1" );
+            $this->fail("An expected exception was not thrown");
+        }
+        catch ( ::cPHP::Exception::DB::Query $err ) {
+            $this->assertSame("Query did not a valid Read result object", $err->getMessage());
+        }
+        
+    }
+    
+    public function testGetRow_Valid ()
+    {
+        
+        $link = $this->getMock(
+                "cPHP::iface::DB::Link",
+                array("query", "quote", "escape")
+            );
+        
+        $query = new ::cPHP::DB::LinkWrap::Querier( $link );
+        
+        $result = $this->getMock(
+                "cPHP::DB::Result::Read",
+                array("rawCount", "rawFetch", "rawSeek", "rawFields", "rawFree"),
+                array(null, "SELECT * FROM table")
+            );
+        
+        $link->expects($this->once())
+            ->method("query")
+            ->with( $this->equalTo("SELECT * FROM table" ) )
+            ->will( $this->returnValue( $result ));
+    
+        
+        $result->expects( $this->once() )
+            ->method("rawSeek")
+            ->with( $this->equalTo(0) )
+            ->will( $this->returnValue(array( 'one', 'two' )));
+        
+        $result->expects( $this->once() )
+            ->method("rawCount")
+            ->will( $this->returnValue(5));
+    
+        $this->assertSame(
+                array( 'one', 'two' ),
+                $query->getRow( "SELECT * FROM table" )
+            );
+        
+    }
+    
+    public function testGetRow_OtherRow ()
+    {
+        
+        $link = $this->getMock(
+                "cPHP::iface::DB::Link",
+                array("query", "quote", "escape")
+            );
+        
+        $query = new ::cPHP::DB::LinkWrap::Querier( $link );
+        
+        $result = $this->getMock(
+                "cPHP::DB::Result::Read",
+                array("rawCount", "rawFetch", "rawSeek", "rawFields", "rawFree"),
+                array(null, "SELECT * FROM table")
+            );
+        
+        $link->expects($this->once())
+            ->method("query")
+            ->with( $this->equalTo("SELECT * FROM table" ) )
+            ->will( $this->returnValue( $result ));
+    
+        
+        $result->expects( $this->once() )
+            ->method("rawSeek")
+            ->with( $this->equalTo(3) )
+            ->will( $this->returnValue(array( 'one', 'two' )));
+        
+        $result->expects( $this->once() )
+            ->method("rawCount")
+            ->will( $this->returnValue(5));
+    
+        $this->assertSame(
+                array( 'one', 'two' ),
+                $query->getRow( "SELECT * FROM table", 3 )
+            );
+        
+    }
+    
+    public function testGetRow_NoResults ()
+    {
+        
+        $link = $this->getMock(
+                "cPHP::iface::DB::Link",
+                array("query", "quote", "escape")
+            );
+        
+        $query = new ::cPHP::DB::LinkWrap::Querier( $link );
+        
+        $result = $this->getMock(
+                "cPHP::DB::Result::Read",
+                array("rawCount", "rawFetch", "rawSeek", "rawFields", "rawFree"),
+                array(null, "SELECT * FROM table")
+            );
+        
+        $link->expects($this->once())
+            ->method("query")
+            ->with( $this->equalTo("SELECT * FROM table" ) )
+            ->will( $this->returnValue( $result ));
+    
+        
+        $result->expects( $this->never() )
+            ->method("rawSeek");
+        
+        $result->expects( $this->once() )
+            ->method("rawCount")
+            ->will( $this->returnValue(0));
+    
+        $this->assertFalse(
+                $query->getRow( "SELECT * FROM table" )
+            );
+        
+    }
+    
+    public function testGetField ()
+    {
+        $this->markTestIncomplete("Not yet written");
     }
     
 }
