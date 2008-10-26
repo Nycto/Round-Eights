ce3fe60fae0afdc61828a1b62a6688239d5579dc
nycto <nycto@e0e46c49-be69-4f5a-ad62-21024a331aea>
2008-10-26 01:41:05 +0000

    Completed database registry
    git-svn-id: http://svn.assembla.com/svn/commonPHP@117 e0e46c49-be69-4f5a-ad62-21024a331aea

--- trunk/src/classes/DB.php
+++ trunk/src/classes/DB.php
@@ -16,6 +16,98 @@ namespace cPHP;
 class DB
 {
     
+    /**
+     * This list of registered connections indexed by a shortcut
+     */
+    static protected $links = array();
+    
+    /**
+     * The default database connection
+     */
+    static protected $default;
+    
+    /**
+     * Returns the full list of database connections
+     *
+     * @return Array
+     */
+    static public function getLinks ()
+    {
+        return self::$links;
+    }
+    
+    /**
+     * Returns the default connection
+     *
+     * @return Object The default connection
+     */
+    static public function getDefault ()
+    {
+        return self::$default;
+    }
+    
+    /**
+     * Registers a new link
+     *
+     * If no default link has been set, the link passed to this instance
+     * will be set as the default
+     *
+     * @param String $label The reference string used to index the connection
+     * @param Object $link The actual database connection
+     * @return Null
+     */
+    static public function setLink( $label, ::cPHP::iface::DB::Link $link )
+    {
+        $label = ::cPHP::strval( $label );
+        
+        if ( ::cPHP::is_empty($label) )
+            throw new ::cPHP::Exception::Argument( 0, "Connection Label", "Must not be empty" );
+        
+        self::$links[ $label ] = $link;
+        
+        if ( !isset(self::$default) )
+            self::setDefault( $label );
+    }
+    
+    /**
+     * Returns a registered link by it's label
+     *
+     * @param String $label The connection to return
+     *      If no label is given, the default connection will be returned
+     * @return Object The default connection
+     */
+    static public function get ( $label = NULL )
+    {
+        if ( !is_string($label) && ::cPHP::is_vague($label) )
+            return self::getDefault();
+        
+        $label = ::cPHP::strval( $label );
+        
+        if ( ::cPHP::is_empty($label) )
+            throw new ::cPHP::Exception::Argument( 0, "Connection Label", "Must not be empty" );
+        
+        if ( !array_key_exists($label, self::$links) )
+            throw new ::cPHP::Exception::Index("Connection Label", $label, "Connection does not exist");
+        
+        return self::$links[$label];
+    }
+    
+    /**
+     * Sets the default connection based on an already registered label
+     *
+     * @param String $label The name of the connection to make the default
+     * @return NULL
+     */
+    static public function setDefault ( $label )
+    {
+        $label = ::cPHP::strval( $label );
+        
+        if ( ::cPHP::is_empty($label) )
+            throw new ::cPHP::Exception::Argument( 0, "Connection Label", "Must not be empty" );
+        
+        self::$default = self::get( $label );
+    }
+    
 }
 
 ?>
