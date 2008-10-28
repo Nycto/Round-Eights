101ea9f34e553da7cbe60368c1248823ae3222eb
Nycto <nycto@nycto-laptop.(none)>
2008-10-28 09:17:23 -0700

    Added digits filter

---
+++ tests/classes/Filter/Digits.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Unit Test File
+ *
+ * PHP version 5.3
+ *
+ * This source file is subject to version 2.0 of the Artistic License. A copy
+ * of the license should have been bundled with this source file inside a file
+ * named LICENSE.txt. It is also available through the world-wide-web at one
+ * of the following URIs:
+ * http://www.commonphp.com/license.php
+ * http://www.opensource.org/licenses/artistic-license-2.0.php
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @copyright Copyright 2008, James Frasca, All Rights Reserved
+ * @author James Frasca <james@commonphp.com>
+ * @license Artistic License 2.0 http://www.commonphp.com/license.php
+ * @package UnitTests
+ */
+
+require_once rtrim( __DIR__, "/" ) ."/../../general.php";
+
+/**
+ * unit tests
+ */
+class classes_filter_digits extends PHPUnit_Framework_TestCase
+{
+    
+    public function testValid ()
+    {
+        $filter = new cPHP::Filter::Digits;
+        
+        $this->assertSame(
+                '1234567890',
+                $filter->filter('1234567890')
+            );
+        
+        $this->assertSame(
+                '0987654321',
+                $filter->filter('0987654321')
+            );
+        
+        $this->assertSame(
+                '1234',
+                $filter->filter(1234)
+            );
+    }
+    
+    public function testInvalidChars ()
+    {
+        $filter = new cPHP::Filter::Digits;
+        
+        $this->assertEquals("", $filter->filter('ABCDEFGHIJKLMNOPQRSTUVWXYZ'));
+        $this->assertEquals("", $filter->filter('abcdefghijklmnopqrstuvwxyz'));
+        $this->assertEquals("", $filter->filter('!"#$%&\'()*+,-/:;<=>?@[\]^`{|}~'));
+        
+        $this->assertEquals(
+                "",
+                $filter->filter(
+                        implode( "", array_map("chr", range(127, 255) ) )
+                    )
+            );
+        
+    }
+    
+}
+
+?>
