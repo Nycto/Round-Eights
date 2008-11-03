b568cdb709d8c2aea9170acb35c6e15a176dea70
Nycto <james@commonphp.com>
2008-11-03 09:09:11 -0800

    Created files, added getOptions and addOption methods

---
+++ src/classes/Form/Options.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * A Basic HTML form field
+ *
+ * PHP version 5.3
+ *
+ * This source file is subject to version 2.0 of the Artistic License. A copy
+ * of the license should have been bundled with this source file inside a file
+ * named LICENSE.txt. It is also available through the world-wide-web at one
+ * of the following URIs:
+ * http://www.commonphp.com/license.php
+ * http://www.opensource.org/licenses/artistic-license-2.0.php
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @copyright Copyright 2008, James Frasca, All Rights Reserved
+ * @author James Frasca <james@commonphp.com>
+ * @license Artistic License 2.0 http://www.commonphp.com/license.php
+ * @package Forms
+ */
+
+namespace cPHP::Form;
+
+/**
+ * The core class for HTML forms
+ */
+abstract class Options extends ::cPHP::Form::Field
+{
+
+    /**
+     * The list of options
+     *
+     * This is an associative array where they key is the option value and
+     * the element value is the option label
+     */
+    private $options = array();
+
+    /**
+     * Returns the list of registered options
+     *
+     * @return Object Returns a cPHP::Ary object
+     */
+    public function getOptions ()
+    {
+        return new ::cPHP::Ary( $this->options );
+    }
+
+    /**
+     * Adds a new option on to this list
+     *
+     * @param mixed $value The raw value of this option. This will be reduced
+     *      down to a basic value
+     * @param String $label The visible label for this value
+     * @return Object Returns a self reference
+     */
+    public function addOption ( $value, $label )
+    {
+        $value = ::cPHP::reduce($value);
+        $label = ::cPHP::strval( $label );
+
+        $this->options[ $value ] = $label;
+
+        return $this;
+    }
+
+}
+
+?>
---
+++ tests/classes/Form/Options.php
@@ -0,0 +1,188 @@
+<?php
+/**
+ * Unit Test File
+ *
+ * PHP version 5.3
+ *
+ * This source file is subject to version 2.0 of the Artistic License. A copy
+ * of the license should have been bundled with this source file inside a file
+ * named LICENSE.txt. It is also available through the world-wide-web at one
+ * of the following URIs:
+ * http://www.commonphp.com/license.php
+ * http://www.opensource.org/licenses/artistic-license-2.0.php
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @copyright Copyright 2008, James Frasca, All Rights Reserved
+ * @author James Frasca <james@commonphp.com>
+ * @license Artistic License 2.0 http://www.commonphp.com/license.php
+ * @package UnitTests
+ */
+
+require_once rtrim( __DIR__, "/" ) ."/../../general.php";
+
+/**
+ * unit tests
+ */
+class classes_form_options extends PHPUnit_Framework_TestCase
+{
+
+    public function testAddOption_strValue ()
+    {
+        $mock = $this->getMock("cPHP::Form::Options", array(), array("fld"));
+
+        $this->assertSame( $mock, $mock->addOption("str", "lbl") );
+
+        $opts = $mock->getOptions();
+        $this->assertThat( $opts, $this->isInstanceOf("cPHP::Ary") );
+        $this->assertSame( array("str" => "lbl"), $opts->get() );
+
+    }
+
+    public function testAddOption_intValue ()
+    {
+        $mock = $this->getMock("cPHP::Form::Options", array(), array("fld"));
+
+        $this->assertSame( $mock, $mock->addOption(50, "lbl") );
+
+        $opts = $mock->getOptions();
+        $this->assertThat( $opts, $this->isInstanceOf("cPHP::Ary") );
+        $this->assertSame( array(50 => "lbl"), $opts->get() );
+
+    }
+
+    public function testAddOption_floatValue ()
+    {
+        $mock = $this->getMock("cPHP::Form::Options", array(), array("fld"));
+
+        $this->assertSame( $mock, $mock->addOption(1.5, "othr") );
+
+        $opts = $mock->getOptions();
+        $this->assertThat( $opts, $this->isInstanceOf("cPHP::Ary") );
+        $this->assertSame(
+                array(1 => "othr"),
+                $opts->get()
+            );
+    }
+
+    public function testAddOption_boolValue ()
+    {
+
+        $mock = $this->getMock("cPHP::Form::Options", array(), array("fld"));
+
+        $this->assertSame( $mock, $mock->addOption(FALSE, "lbl") );
+
+        $opts = $mock->getOptions();
+        $this->assertThat( $opts, $this->isInstanceOf("cPHP::Ary") );
+        $this->assertSame(
+                array(0 => "lbl"),
+                $opts->get()
+            );
+
+        $this->assertSame( $mock, $mock->addOption(TRUE, "lbl2") );
+
+        $opts = $mock->getOptions();
+        $this->assertThat( $opts, $this->isInstanceOf("cPHP::Ary") );
+        $this->assertSame(
+                array(0 => "lbl", 1 => "lbl2"),
+                $opts->get()
+            );
+    }
+
+    public function testAddOption_nullValue ()
+    {
+
+        $mock = $this->getMock("cPHP::Form::Options", array(), array("fld"));
+
+        $this->assertSame( $mock, $mock->addOption(null, "lbl") );
+
+        $opts = $mock->getOptions();
+        $this->assertThat( $opts, $this->isInstanceOf("cPHP::Ary") );
+        $this->assertSame(
+                array("" => "lbl"),
+                $opts->get()
+            );
+
+    }
+
+    public function testAddOption_objValue ()
+    {
+
+        $mock = $this->getMock("cPHP::Form::Options", array(), array("fld"));
+
+        $this->assertSame( $mock, $mock->addOption( $this->getMock("stub"), "lbl") );
+
+        $opts = $mock->getOptions();
+        $this->assertThat( $opts, $this->isInstanceOf("cPHP::Ary") );
+        $this->assertSame(
+                array("" => "lbl"),
+                $opts->get()
+            );
+
+    }
+
+    public function testAddOption_nonStringLabel ()
+    {
+
+        $mock = $this->getMock("cPHP::Form::Options", array(), array("fld"));
+
+        $this->assertSame( $mock, $mock->addOption( 1, 5) );
+
+        $opts = $mock->getOptions();
+        $this->assertThat( $opts, $this->isInstanceOf("cPHP::Ary") );
+        $this->assertSame(
+                array(1 => "5"),
+                $opts->get()
+            );
+
+        $this->assertSame( $mock, $mock->addOption( 2, 27.8) );
+
+        $opts = $mock->getOptions();
+        $this->assertThat( $opts, $this->isInstanceOf("cPHP::Ary") );
+        $this->assertSame(
+                array(1 => "5", 2 => "27.8"),
+                $opts->get()
+            );
+
+    }
+
+    public function testAddOption_conflict ()
+    {
+
+        $mock = $this->getMock("cPHP::Form::Options", array(), array("fld"));
+
+        $this->assertSame( $mock, $mock->addOption( "val", "one") );
+
+        $opts = $mock->getOptions();
+        $this->assertThat( $opts, $this->isInstanceOf("cPHP::Ary") );
+        $this->assertSame(
+                array("val" => "one"),
+                $opts->get()
+            );
+
+
+        $this->assertSame( $mock, $mock->addOption( "val", "two") );
+
+        $opts = $mock->getOptions();
+        $this->assertThat( $opts, $this->isInstanceOf("cPHP::Ary") );
+        $this->assertSame(
+                array("val" => "two"),
+                $opts->get()
+            );
+
+    }
+
+}
+
+?>
