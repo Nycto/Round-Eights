6403c9790e6cf3dad7fdf8e54db964e401f30133
Nycto <james@commonphp.com>
2008-12-12 09:10:06 -0800

    Created Dir Test Framework, switched FileSys\Dir tests over to using it

--- tests/general.php
+++ tests/general.php
@@ -327,6 +327,114 @@ abstract class PHPUnit_TestFile_Framework_TestCase extends PHPUnit_EmptyFile_Fra
 }
 
 /**
+ * Base test class for tests that use temporary files/directories
+ */
+abstract class PHPUnit_Dir_Framework_TestCase extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * The temporary directory that was created
+     */
+    protected $dir;
+
+    /**
+     * Creates a new temporary directory with a set of fake files in it
+     */
+    public function setUp ()
+    {
+        $this->dir = rtrim( sys_get_temp_dir(), "/" ) ."/cPHP_". uniqid();
+
+        if (!mkdir( $this->dir ))
+            $this->markTestSkipped("Unable to create temporary directory: ". $this->dir);
+
+        $toCreate = array(
+                "first/.",
+                "second/second-one",
+                "third/third-one",
+                "third/third-two",
+                "third/third-three",
+                "third/fourth/.",
+                "third/fourth/fourth-one",
+                "third/fourth/fourth-two",
+                "one",
+                "two",
+                "three",
+                "four",
+            );
+
+        foreach ( $toCreate AS $path ) {
+
+            $dirname = dirname($path);
+
+            if ( $dirname != "." ) {
+
+                $dirname = $this->dir ."/". $dirname;
+
+                if ( !is_dir($dirname) && !mkdir($dirname, 0777) )
+                    $this->markTestSkipped("Unable to create temporary dir: ". $dirname );
+
+            }
+
+            if ( basename($path) != "." ) {
+
+                $basename = $this->dir ."/". $path;
+
+                if ( !touch($basename) )
+                    $this->markTestSkipped("Unable to create temporary file: ". $basename );
+
+                @chmod( $basename, 0777 );
+            }
+
+        }
+
+    }
+
+    /**
+     * Deletes a given path and everything in it
+     */
+    private function delete ( $path )
+    {
+
+        if ( is_file($path) ) {
+            @chmod($path, 0777);
+            @unlink($path);
+        }
+
+        else if( is_dir($path) ) {
+
+            @chmod($path, 0777);
+
+            foreach( new DirectoryIterator($path) as $item ) {
+
+                if( $item->isDot() )
+                    continue;
+
+                if( $item->isFile() )
+                    $this->delete( $item->getPathName() );
+
+                else if( $item->isDir() )
+                    $this->delete( $item->getRealPath() );
+
+                unset($_res);
+            }
+
+            @rmdir( $path );
+
+        }
+
+    }
+
+    /**
+     * Deletes the temporary files
+     */
+    public function tearDown ()
+    {
+        $this->delete( $this->dir );
+    }
+
+}
+
+/**
  * Stub of the Env class that allows tests to create an instance that represents
  * a specific environment
  */
