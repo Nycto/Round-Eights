57732397ba6c84c9febceb6fdc237cc9919f15b9
Nycto <james@commonphp.com>
2008-12-20 11:12:38 -0800

    Initial implementation of Template\Replace

--- src/classes/Template/Replace.php
+++ src/classes/Template/Replace.php
@@ -29,9 +29,129 @@ namespace cPHP\Template;
 
 /**
  * A basic search and replace template
+ *
+ * The default replacement string is #{label}
  */
 class Replace extends \cPHP\Template
 {
+
+    /**
+     * The search string to replace
+     */
+    protected $search = '/(\\\\*)(#\{(.*?)\})/';
+
+    /**
+     * The template string that will be rendered
+     */
+    protected $template;
+
+    /**
+     * Constructor...
+     *
+     * @param String $template The template string that will be rendered
+     * @param mixed $import An array, object, or another template to use as the
+     *      source data. See the import method for more information.
+     */
+    public function __construct ( $template, $import = null )
+    {
+        parent::__construct( $import );
+        $this->setTemplate( $template );
+    }
+
+    /**
+     * Returns the template string that will be rendered
+     *
+     * @return String The template string that will be rendered
+     */
+    public function getTemplate ()
+    {
+        return $this->template;
+    }
+
+    /**
+     * Sets the template that will be rendered
+     *
+     * @param String $template The template string
+     * @return Object Returns a self reference
+     */
+    public function setTemplate ( $template )
+    {
+        $this->template = \cPHP\strval( $template );
+        return $this;
+    }
+
+    /**
+     * Returns the regular expression that will be used to find replacements
+     *
+     * @return String Returns the search string
+     */
+    public function getSearch ()
+    {
+        return $this->search;
+    }
+
+    /**
+     * Sets the regular expression that will be used to find replacements
+     *
+     * To make sure escaping works, this regular expression must return three
+     * groupings. The first is to capture any preceding escape characters. The
+     * second will capture the string to be replaced. The third group captures
+     * the label to lookup the new value.
+     *
+     * @param String $search The search string
+     * @return Object Returns a self reference
+     */
+    public function setSearch ( $search )
+    {
+        $this->search = \cPHP\strval( $search );
+        return $this;
+    }
+
+    /**
+     * Renders this template and outputs it to the client
+     *
+     * @return Object Returns a self reference
+     */
+    public function display ()
+    {
+        echo $this->render();
+        return $this;
+    }
+
+    /**
+     * Renders the template and returns it as a string
+     *
+     * @return String Returns the rendered template as a string
+     */
+    public function render ()
+    {
+        return preg_replace_callback( $this->search, function ($matches) {
+
+            // Ensure that the search string returns the proper number of matches
+            if ( count($matches) < 4 ) {
+                $err = new \cPHP\Exception\Data(
+                        $this->search,
+                        "Search Regular Expression",
+                        "Must return at least 3 groupings"
+                    );
+                $err->addData("Groupings", count($matches) - 1);
+                throw $err;
+            }
+
+            // Handle escaped replace strings
+            if ( strlen($matches[1]) % 2 != 0 )
+                return substr( $matches[1], 0, -1 ) .$matches[2];
+
+            // If this variable isn't set or this replacement would cause recursion, return blank
+            else if ( !$this->exists( $matches[3] ) || $this->get( $matches[3] ) === $this )
+                return $matches[1];
+
+            // Otherwise make the replacement
+            else
+                return $matches[1] . \cPHP\strval( $this->get( $matches[3] ) );
+
+        }, $this->template );
+    }
 
 }
 
--- tests/classes/Template/Replace.php
+++ tests/classes/Template/Replace.php
@@ -28,10 +28,118 @@
 require_once rtrim( __DIR__, "/" ) ."/../../general.php";
 
 /**
+ * Test Suite
+ */
+class classes_template_raw
+{
+
+    public static function suite()
+    {
+        $suite = new cPHP_Base_TestSuite;
+        $suite->addTestSuite( 'classes_template_replace_standard' );
+        $suite->addTestSuite( 'classes_template_replace_output' );
+        return $suite;
+    }
+
+}
+
+/**
  * unit tests
  */
-class classes_template_replace extends PHPUnit_Framework_TestCase
+class classes_template_replace_standard extends PHPUnit_Framework_TestCase
 {
+
+    public function testTemplateAccessors ()
+    {
+        $tpl = new \cPHP\Template\Replace("tpl");
+
+        $this->assertSame( "tpl", $tpl->getTemplate() );
+        $this->assertSame( $tpl, $tpl->setTemplate("new template") );
+        $this->assertSame( "new template", $tpl->getTemplate() );
+    }
+
+    public function testSearchAccessors ()
+    {
+        $tpl = new \cPHP\Template\Replace("tpl");
+
+        $this->assertSame( '/(\\\\*)(#\{(.*?)\})/', $tpl->getSearch() );
+        $this->assertSame( $tpl, $tpl->setSearch('/replace/') );
+        $this->assertSame( '/replace/', $tpl->getSearch() );
+    }
+
+    public function testRender ()
+    {
+        $tpl = new \cPHP\Template\Replace("#{adjective} #{noun} #{verb}");
+        $tpl->import(array(
+                "adjective" => "quick",
+                "noun" => "fox",
+                "verb" => "ran"
+            ));
+        $this->assertSame( "quick fox ran", $tpl->render() );
+    }
+
+    public function testRender_escaped ()
+    {
+        $tpl = new \cPHP\Template\Replace('#{test} \\#{test} \\\\#{test} \\\\\\#{test}');
+        $tpl->set('test', 'value');
+
+        $this->assertSame( "value #{test} \\\\value \\\\#{test}", $tpl->render() );
+    }
+
+    public function testRender_empty ()
+    {
+        $tpl = new \cPHP\Template\Replace("The #{adjective} #{noun} #{verb}");
+        $this->assertSame( "The   ", $tpl->render() );
+    }
+
+    public function testRender_recursion ()
+    {
+        $tpl = new \cPHP\Template\Replace("#{value}");
+        $tpl->set('value', $tpl);
+        $this->assertSame( "", $tpl->render() );
+    }
+
+    public function testNewSearch ()
+    {
+        $tpl = new \cPHP\Template\Replace('<b><%=bold %></b>');
+        $tpl->setSearch("/(\\\\*)(\<%=\s*(\w+)\s*%\>)/");
+        $tpl->set('bold', 'STRONG');
+
+        $this->assertSame( "<b>STRONG</b>", $tpl->render() );
+    }
+
+    public function testWrongGroupings ()
+    {
+        $tpl = new \cPHP\Template\Replace('replace');
+        $tpl->setSearch("/replace/");
+
+        try {
+            $tpl->render();
+            $this->fail("An expected exception was not thrown");
+        }
+        catch ( \cPHP\Exception\Data $err ) {
+            $this->assertSame( "Must return at least 3 groupings", $err->getMessage() );
+        }
+    }
+
+}
+
+class classes_template_replace_output extends PHPUnit_Extensions_OutputTestCase
+{
+
+    public function testDisplay ()
+    {
+        $this->expectOutputString("The quick fox ran");
+
+        $tpl = new \cPHP\Template\Replace("The #{adjective} #{noun} #{verb}");
+        $tpl->import(array(
+                "adjective" => "quick",
+                "noun" => "fox",
+                "verb" => "ran"
+            ));
+
+        $this->assertSame( $tpl, $tpl->display() );
+    }
 
 }
 
