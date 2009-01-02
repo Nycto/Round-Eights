e17a9ff81bef3725f5bdb7be614d5d62be65dde5
Nycto <james@commonphp.com>
2009-01-01 17:46:42 -0800

    Added Curry\Invoke

---
+++ src/classes/Curry/Invoke.php
@@ -0,0 +1,143 @@
+<?php
+/**
+ * Function Currying
+ *
+ * @license Artistic License 2.0
+ *
+ * This file is part of commonPHP.
+ *
+ * commonPHP is free software: you can redistribute it and/or modify
+ * it under the terms of the Artistic License as published by
+ * the Open Source Initiative, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * commonPHP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Artistic License for more details.
+ *
+ * You should have received a copy of the Artistic License
+ * along with commonPHP. If not, see <http://www.commonphp.com/license.php>
+ * or <http://www.opensource.org/licenses/artistic-license-2.0.php>.
+ *
+ * @author James Frasca <james@commonphp.com>
+ * @copyright Copyright 2008, James Frasca, All Rights Reserved
+ * @package curry
+ */
+
+namespace cPHP\Curry;
+
+/**
+ * A curry class for invoking object methods
+ */
+class Invoke extends \cPHP\Curry
+{
+
+    /**
+     * The name of the method to invoke
+     */
+    protected $method;
+
+    /**
+     * Constructor...
+     *
+     * @param String $method The name of the method to invoke
+     * @param mixed $args... Any rightward arguments
+     */
+    public function __construct ( $method )
+    {
+        $method = trim( \cPHP\strVal( $method ) );
+
+        if ( !\cPHP\Validator::Method()->isValid($method) )
+            throw new \cPHP\Exception\Argument( 0, "Method", "Invalid method name" );
+
+        $this->method = $method;
+
+        if ( func_num_args() > 1 ) {
+            $args = func_get_args();
+            array_shift( $args );
+            $this->setRightByArray( $args );
+        }
+    }
+
+    /**
+     * Instantiates the current class with the given array of arguments and returns the new object
+     *
+     * @param Object $object The object to invoke this method against
+     * @param $args Array The arguments to apply to the callback
+     * @return Object Returns a new instance
+     */
+    protected function rawExec ( $object, array $args = array() )
+    {
+        if ( !is_object($object) )
+            throw new \cPHP\Exception\Argument( 0, "Object", "Must be an object" );
+
+        // Take the easy outs if we can
+        if ( count($args) <= 0 )
+            return $object->{ $this->method }();
+
+        else if ( count($args) == 1 )
+            return $object->{ $this->method } ( reset($args) );
+
+        return call_user_func_array(
+                array( $object, $this->method ),
+                $args
+            );
+    }
+
+    /**
+     * Invokes this method against a given object using the contents of an array
+     * as the arguments
+     *
+     * @param Object $object The object whose method should be invoked
+     * @param array $args The list of arguments to apply to this function
+     * @return mixed Returns the results of the function call
+     */
+    public function apply ( $object, array $args = array() )
+    {
+        return $this->rawExec( $object, $this->collectArgs($args) );
+    }
+
+    /**
+     * Invokes this method against the given object with the given arguments
+     *
+     * @param Object $object The object whose method should be invoked
+     * @param mixed $args... The arguments to pass to the callback
+     * @return mixed Returns the result of the invokation
+     */
+    public function exec ( $object )
+    {
+        $args = func_get_args();
+        array_shift( $args );
+        return $this->rawExec( $object, $this->collectArgs($args) );
+    }
+
+    /**
+     * Invokes this method against the given object with the given arguments
+     *
+     * @param Object $object The object whose method should be invoked
+     * @param mixed $args... Any arguments to apply to the function
+     * @return mixed Returns the results of the invokation
+     */
+    public function __invoke ( $object )
+    {
+        $args = func_get_args();
+        array_shift( $args );
+        return $this->rawExec( $object, $this->collectArgs($args) );
+    }
+
+    /**
+     * Method for use with the filtering objects. Invokes the contained method
+     * against the given object
+     *
+     * @param Object $object The object whose method should be invoked
+     * @return mixed The result of the filtering
+     */
+    public function filter ( $object )
+    {
+        return $this->rawExec( $object, $this->collectArgs(array()) );
+    }
+
+}
+
+?>
---
+++ tests/classes/Curry/Invoke.php
@@ -0,0 +1,278 @@
+<?php
+/**
+ * Unit Test File
+ *
+ * @license Artistic License 2.0
+ *
+ * This file is part of commonPHP.
+ *
+ * commonPHP is free software: you can redistribute it and/or modify
+ * it under the terms of the Artistic License as published by
+ * the Open Source Initiative, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * commonPHP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Artistic License for more details.
+ *
+ * You should have received a copy of the Artistic License
+ * along with commonPHP. If not, see <http://www.commonphp.com/license.php>
+ * or <http://www.opensource.org/licenses/artistic-license-2.0.php>.
+ *
+ * @author James Frasca <james@commonphp.com>
+ * @copyright Copyright 2008, James Frasca, All Rights Reserved
+ * @package UnitTests
+ */
+
+require_once rtrim( __DIR__, "/" ) ."/../../general.php";
+
+/**
+ * unit tests
+ */
+class classes_curry_invoke extends PHPUnit_Framework_TestCase
+{
+
+    public function testConstructError ()
+    {
+        try {
+            new \cPHP\Curry\Invoke( "This is not a valid method" );
+            $this->fail("An expected exception was not thrown");
+        }
+        catch ( \cPHP\Exception\Argument $err ) {
+            $this->assertSame( "Invalid method name", $err->getMessage() );
+        }
+    }
+
+    public function testConstructArgs ()
+    {
+        $curry = new \cPHP\Curry\Invoke( "methodName" );
+        $this->assertEquals( array(), $curry->getRight() );
+        $this->assertEquals( array(), $curry->getLeft() );
+
+        $curry = new \cPHP\Curry\Invoke( "methodName", "one", "two" );
+        $this->assertEquals( array("one", "two"), $curry->getRight() );
+        $this->assertEquals( array(), $curry->getLeft() );
+    }
+
+    public function testExec_noArgs ()
+    {
+        $obj = $this->getMock('stdClass', array('method'));
+        $obj->expects( $this->once() )
+            ->method('method')
+            ->will( $this->returnValue("Result") );
+
+        $curry = new \cPHP\Curry\Invoke('method');
+
+        $this->assertSame(
+                "Result",
+                $curry->exec($obj)
+            );
+    }
+
+    public function testExec_oneArgs ()
+    {
+        $obj = $this->getMock('stdClass', array('method'));
+        $obj->expects( $this->once() )
+            ->method('method')
+            ->with(
+                    $this->equalTo( "wakka" )
+                )
+            ->will( $this->returnValue("Result") );
+
+        $curry = new \cPHP\Curry\Invoke('method');
+
+        $this->assertSame(
+                "Result",
+                $curry->exec($obj, "wakka")
+            );
+    }
+
+    public function testExec_manyArgs ()
+    {
+        $obj = $this->getMock('stdClass', array('method'));
+        $obj->expects( $this->once() )
+            ->method('method')
+            ->with(
+                    $this->equalTo( "wakka" ),
+                    $this->equalTo( "test1")
+                )
+            ->will( $this->returnValue("Result") );
+
+        $curry = new \cPHP\Curry\Invoke('method');
+
+        $this->assertSame(
+                "Result",
+                $curry->exec($obj, "wakka", "test1")
+            );
+    }
+
+    public function testExecWithLeftRight ()
+    {
+        $obj = $this->getMock('stdClass', array('method'));
+        $obj->expects( $this->once() )
+            ->method('method')
+            ->with(
+                    $this->equalTo("l1"),
+                    $this->equalTo("l2"),
+                    $this->equalTo(1),
+                    $this->equalTo(2),
+                    $this->equalTo(3),
+                    $this->equalTo("r1"),
+                    $this->equalTo("r2")
+                )
+            ->will( $this->returnValue("Result") );
+
+
+        $curry = new \cPHP\Curry\Invoke('method');
+        $curry->setLeft("l1", "l2");
+        $curry->setRight("r1", "r2");
+
+
+        $this->assertSame(
+                "Result",
+                $curry->exec($obj, 1, 2, 3)
+            );
+    }
+
+    public function testApply ()
+    {
+        $obj = $this->getMock('stdClass', array('method'));
+        $obj->expects( $this->once() )
+            ->method('method')
+            ->with(
+                    $this->equalTo("wakka"),
+                    $this->equalTo("test1")
+                )
+            ->will( $this->returnValue("Result") );
+
+
+        $curry = new \cPHP\Curry\Invoke('method');
+
+
+        $this->assertSame(
+                "Result",
+                $curry->apply($obj, array("wakka", "test1"))
+            );
+
+    }
+
+    public function testApplyWithLeftRight ()
+    {
+        $obj = $this->getMock('stdClass', array('method'));
+        $obj->expects( $this->once() )
+            ->method('method')
+            ->with(
+                    $this->equalTo("l1"),
+                    $this->equalTo("wakka"),
+                    $this->equalTo("test1"),
+                    $this->equalTo("r1")
+                )
+            ->will( $this->returnValue("Result") );
+
+
+        $curry = new \cPHP\Curry\Invoke('method');
+        $curry->setLeft("l1");
+        $curry->setRight("r1");
+
+
+        $this->assertSame(
+                "Result",
+                $curry->apply($obj, array("wakka", "test1"))
+            );
+
+    }
+
+    public function testInvoke ()
+    {
+        $obj = $this->getMock('stdClass', array('method'));
+        $obj->expects( $this->once() )
+            ->method('method')
+            ->with(
+                    $this->equalTo("wakka"),
+                    $this->equalTo("test1")
+                )
+            ->will( $this->returnValue("Result") );
+
+
+        $curry = new \cPHP\Curry\Invoke('method');
+
+
+        $this->assertSame(
+                "Result",
+                $curry($obj, "wakka", "test1")
+            );
+
+    }
+
+    public function testInvokeWithLeftRight ()
+    {
+        $obj = $this->getMock('stdClass', array('method'));
+        $obj->expects( $this->once() )
+            ->method('method')
+            ->with(
+                    $this->equalTo("l1"),
+                    $this->equalTo("wakka"),
+                    $this->equalTo("test1"),
+                    $this->equalTo("r1")
+                )
+            ->will( $this->returnValue("Result") );
+
+
+        $curry = new \cPHP\Curry\Invoke('method');
+        $curry->setLeft("l1");
+        $curry->setRight("r1");
+
+
+        $this->assertSame(
+                "Result",
+                $curry($obj, "wakka", "test1")
+            );
+
+    }
+
+    public function testFilter ()
+    {
+
+        $obj = $this->getMock('stdClass', array('method'));
+        $obj->expects( $this->once() )
+            ->method('method')
+            ->with()
+            ->will( $this->returnValue("Result") );
+
+
+        $curry = new \cPHP\Curry\Invoke('method');
+
+
+        $this->assertSame(
+                "Result",
+                $curry->filter($obj)
+            );
+    }
+
+    public function testFilterWithLeftRight ()
+    {
+        $obj = $this->getMock('stdClass', array('method'));
+        $obj->expects( $this->once() )
+            ->method('method')
+            ->with(
+                    $this->equalTo("l1"),
+                    $this->equalTo("r1")
+                )
+            ->will( $this->returnValue("Result") );
+
+
+        $curry = new \cPHP\Curry\Invoke('method');
+        $curry->setLeft("l1");
+        $curry->setRight("r1");
+
+
+        $this->assertSame(
+                "Result",
+                $curry->filter($obj)
+            );
+    }
+
+}
+
+?>
