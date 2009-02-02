34b523bad2887de430716234318d7848c889a72a
Nycto <james@commonphp.com>
2009-02-02 08:55:45 -0800

    Added Encode\Base64

---
+++ src/classes/Encode/Base64.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Encodes a string using base 64
+ *
+ * @license Artistic License 2.0
+ *
+ * This file is part of commonPHP.
+ *
+ * commonPHP is free software: you can redistribute it and/or modify
+ * it under the terms of the Artistic License as published by
+ * the Open Source Initiative, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * commonPHP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Artistic License for more details.
+ *
+ * You should have received a copy of the Artistic License
+ * along with commonPHP. If not, see <http://www.commonphp.com/license.php>
+ * or <http://www.opensource.org/licenses/artistic-license-2.0.php>.
+ *
+ * @author James Frasca <james@commonphp.com>
+ * @copyright Copyright 2008, James Frasca, All Rights Reserved
+ * @package Encoding
+ */
+
+namespace cPHP\Encode;
+
+/**
+ * Encodes and decodes strings according to the base 64 specification
+ */
+class Base64 implements \cPHP\iface\Encoder
+{
+
+    /**
+     * Encodes a string
+     *
+     * @param mixed $value The value to encode
+     * @result mixed The result of the encoding process
+     */
+    public function encode ( $string )
+    {
+        return base64_encode( \cPHP\strval($string) );
+    }
+
+    /**
+     * Decodes an encoded string
+     *
+     * @param mixed $value The value to decode
+     * @result mixed The original, unencoded value
+     */
+    public function decode ( $string )
+    {
+        return base64_decode( \cPHP\strval($string) );
+    }
+
+}
+
+?>
---
+++ tests/classes/Encode/Base64.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Unit Test File
+ *
+ * @license Artistic License 2.0
+ *
+ * This file is part of commonPHP.
+ *
+ * commonPHP is free software: you can redistribute it and/or modify
+ * it under the terms of the Artistic License as published by
+ * the Open Source Initiative, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * commonPHP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Artistic License for more details.
+ *
+ * You should have received a copy of the Artistic License
+ * along with commonPHP. If not, see <http://www.commonphp.com/license.php>
+ * or <http://www.opensource.org/licenses/artistic-license-2.0.php>.
+ *
+ * @author James Frasca <james@commonphp.com>
+ * @copyright Copyright 2008, James Frasca, All Rights Reserved
+ * @package UnitTests
+ */
+
+require_once rtrim( __DIR__, "/" ) ."/../../general.php";
+
+/**
+ * unit tests
+ */
+class classes_encode_base64 extends PHPUnit_Framework_TestCase
+{
+
+    public function testEncode ()
+    {
+        $encode = new \cPHP\Encode\Base64;
+
+        $this->assertSame(
+                "VGhpcyBpcyBhIHN0cmluZw==",
+                $encode->encode("This is a string")
+            );
+    }
+
+    public function testDecode ()
+    {
+        $encode = new \cPHP\Encode\Base64;
+
+        $this->assertSame(
+                "This is a string",
+                $encode->decode("VGhpcyBpcyBhIHN0cmluZw==")
+            );
+    }
+
+}
+
+?>
