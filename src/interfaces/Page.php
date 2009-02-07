3bd01353d2dc2d8a83e5c3fc219dd912f8277553
Nycto <james@commonphp.com>
2009-02-06 18:42:50 -0800

    Added Page interface

---
+++ src/interfaces/Page.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Page Encapsulation interface
+ *
+ * @license Artistic License 2.0
+ *
+ * This file is part of commonPHP.
+ *
+ * commonPHP is free software: you can redistribute it and/or modify
+ * it under the terms of the Artistic License as published by
+ * the Open Source Initiative, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * commonPHP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Artistic License for more details.
+ *
+ * You should have received a copy of the Artistic License
+ * along with commonPHP. If not, see <http://www.commonphp.com/license.php>
+ * or <http://www.opensource.org/licenses/artistic-license-2.0.php>.
+ *
+ * @author James Frasca <james@commonphp.com>
+ * @copyright Copyright 2008, James Frasca, All Rights Reserved
+ * @package FileFinder
+ */
+
+namespace cPHP\iface;
+
+/**
+ * General interface for creating a page
+ */
+interface Page
+{
+
+    /**
+     * Outputs this page to the user
+     *
+     * @return Object Returns a self reference
+     */
+    public function display();
+
+    /**
+     * Returns the content of this page as a string
+     *
+     * @return String
+     */
+    public function render();
+
+}
+
+?>
