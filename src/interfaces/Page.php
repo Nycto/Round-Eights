fbd6d24e1a25b278bfe99fe0701ead99284675a4
Nycto <james@commonphp.com>
2009-03-12 08:01:17 -0700

    Updated page interface to reflect new design

--- src/interfaces/Page.php
+++ src/interfaces/Page.php
@@ -28,24 +28,19 @@
 namespace cPHP\iface;
 
 /**
- * General interface for creating a page
+ * Interface for a node in a page wire frame
  */
 interface Page
 {
 
     /**
-     * Outputs this page to the user
-     *
-     * @return Object Returns a self reference
-     */
-    public function display();
-
-    /**
      * Returns the content of this page as a string
      *
-     * @return String
+     * @param cPHP\Page $page The core page that will be used to synchronize
+     *      the actions of all the other pages
+     * @return cPHP\iface\Template
      */
-    public function render();
+    public function getTemplate ( \cPHP\Page $page );
 
 }
 
