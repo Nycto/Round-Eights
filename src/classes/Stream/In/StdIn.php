94fb3402fb25672c663d5689040a88b2a166bd4f
Nycto <james@commonphp.com>
2009-05-15 08:18:24 -0700

    Added Stream\In\StdIn

---
+++ src/classes/Stream/In/StdIn.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @license Artistic License 2.0
+ *
+ * This file is part of commonPHP.
+ *
+ * commonPHP is free software: you can redistribute it and/or modify
+ * it under the terms of the Artistic License as published by
+ * the Open Source Initiative, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * commonPHP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Artistic License for more details.
+ *
+ * You should have received a copy of the Artistic License
+ * along with commonPHP. If not, see <http://www.commonphp.com/license.php>
+ * or <http://www.opensource.org/licenses/artistic-license-2.0.php>.
+ *
+ * @author James Frasca <james@commonphp.com>
+ * @copyright Copyright 2008, James Frasca, All Rights Reserved
+ * @package Stream
+ */
+
+namespace cPHP\Stream\In;
+
+/**
+ * A Stream for reading from Standard In (StdIn)
+ *
+ * This can be used to read the raw value Posted by a client
+ */
+class StdIn extends \cPHP\Stream\In\URI
+{
+
+    /**
+     * Constructor...
+     */
+    public function __construct ()
+    {
+        parent::__construct( "php://stdin" );
+    }
+
+}
+
+?>
