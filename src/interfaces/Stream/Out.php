f6d56db97d4c552d7a95b8905d7b1e4c27506c77
Nycto <james@commonphp.com>
2009-05-16 10:37:00 -0700

    Added iface\Stream\Out

---
+++ src/interfaces/Stream/Out.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @license Artistic License 2.0
+ *
+ * This file is part of commonPHP.
+ *
+ * commonPHP is free software: you can redistribute it and/or modify
+ * it under the terms of the Artistic License as published by
+ * the Open Source Initiative, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * commonPHP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Artistic License for more details.
+ *
+ * You should have received a copy of the Artistic License
+ * along with commonPHP. If not, see <http://www.commonphp.com/license.php>
+ * or <http://www.opensource.org/licenses/artistic-license-2.0.php>.
+ *
+ * @author James Frasca <james@commonphp.com>
+ * @copyright Copyright 2008, James Frasca, All Rights Reserved
+ * @package Stream
+ */
+
+namespace cPHP\iface\Stream;
+
+/**
+ * Output stream interface
+ */
+interface Out
+{
+
+    /**
+     * Returns whether there is any more information to read from this stream
+     *
+     * @param String $data The string of data to to write to this stream
+     * @return \cPHP\iface\Stream\Out Returns a self reference
+     */
+    public function write ( $data );
+
+}
+
+?>
