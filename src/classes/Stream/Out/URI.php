a4e1466f2b48624d87e4795dfb08b4e617fa395c
Nycto <james@commonphp.com>
2009-05-16 12:19:48 -0700

    Implemented Stream\Out\URI

---
+++ src/classes/Stream/Out/URI.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * @license Artistic License 2.0
+ *
+ * This file is part of commonPHP.
+ *
+ * commonPHP is free software: you can redistribute it and/or modify
+ * it under the terms of the Artistic License as published by
+ * the Open Source Initiative, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * commonPHP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Artistic License for more details.
+ *
+ * You should have received a copy of the Artistic License
+ * along with commonPHP. If not, see <http://www.commonphp.com/license.php>
+ * or <http://www.opensource.org/licenses/artistic-license-2.0.php>.
+ *
+ * @author James Frasca <james@commonphp.com>
+ * @copyright Copyright 2008, James Frasca, All Rights Reserved
+ * @package Stream
+ */
+
+namespace cPHP\Stream\Out;
+
+/**
+ * Provides a Stream interface for a URI string
+ */
+class URI implements \cPHP\iface\Stream\Out
+{
+
+    /**
+     * The stream resource to read from
+     *
+     * @var Resource
+     */
+    private $resource;
+
+    /**
+     * Constructor...
+     *
+     * @param String $uri The URI to open
+     * @param Boolean $append Whether to append to the end of the URI
+     */
+    public function __construct ( $uri, $append = FALSE )
+    {
+        $uri = \cPHP\strval( $uri );
+
+        $this->resource = @fopen(
+                $uri,
+                $append ? "a" : "w"
+            );
+
+        if ( $this->resource === FALSE ) {
+            throw new \cPHP\Exception\FileSystem\Permissions(
+                    $uri,
+                    "Could not open URI for writing"
+                );
+        }
+    }
+
+    /**
+     * Destructor...
+     *
+     * @return null
+     */
+    public function __destruct ()
+    {
+        $this->close();
+    }
+
+    /**
+     * Closes this resource
+     *
+     * @return \cPHP\iface\Stream\In\URI Returns a self reference
+     */
+    public function close ()
+    {
+        if ( is_resource($this->resource) ) {
+            fclose( $this->resource );
+            $this->resource = null;
+        }
+
+        return $this;
+    }
+
+    /**
+     * Returns whether there is any more information to read from this stream
+     *
+     * @param String $data The string of data to to write to this stream
+     * @return \cPHP\Stream\Out\URI Returns a self reference
+     */
+    public function write ( $data )
+    {
+        $data = \cPHP\strval( $data );
+
+        if ( is_resource($this->resource) )
+            fwrite( $this->resource, $data );
+
+        return $this;
+    }
+
+}
+
+?>
---
+++ tests/classes/Stream/Out/URI.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Unit Test File
+ *
+ * @license Artistic License 2.0
+ *
+ * This file is part of commonPHP.
+ *
+ * commonPHP is free software: you can redistribute it and/or modify
+ * it under the terms of the Artistic License as published by
+ * the Open Source Initiative, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * commonPHP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Artistic License for more details.
+ *
+ * You should have received a copy of the Artistic License
+ * along with commonPHP. If not, see <http://www.commonphp.com/license.php>
+ * or <http://www.opensource.org/licenses/artistic-license-2.0.php>.
+ *
+ * @author James Frasca <james@commonphp.com>
+ * @copyright Copyright 2008, James Frasca, All Rights Reserved
+ * @package UnitTests
+ */
+
+require_once rtrim( __DIR__, "/" ) ."/../../../general.php";
+
+/**
+ * unit tests
+ */
+class classes_stream_out_uri extends PHPUnit_TestFile_Framework_TestCase
+{
+
+    public function testNoPermissiosn ()
+    {
+        chmod( $this->file, 0000 );
+
+        try {
+            new \cPHP\Stream\Out\URI( $this->file );
+            $this->fail("An expected exception was not thrown");
+        }
+        catch ( \cPHP\Exception\FileSystem\Permissions $err ) {
+            $this->assertSame(
+                    "Could not open URI for writing",
+                    $err->getMessage()
+                );
+        }
+    }
+
+    public function testWrite ()
+    {
+        $stream = new \cPHP\Stream\Out\URI( $this->file );
+
+        $this->assertSame( $stream, $stream->write("Data") );
+
+        $stream->close();
+
+        $this->assertSame(
+                "Data",
+                file_get_contents( $this->file )
+            );
+    }
+
+    public function testAppend ()
+    {
+        $stream = new \cPHP\Stream\Out\URI( $this->file, TRUE );
+
+        $this->assertSame( $stream, $stream->write("\nData") );
+
+        $stream->close();
+
+        $this->assertSame(
+                "This is a string\n"
+                ."of data that is put\n"
+                ."in the test file\n"
+                ."Data",
+                file_get_contents( $this->file )
+            );
+    }
+
+}
+
+?>
