d2e5ae81fb38b383a61e2d188aa24a0cdff95332
Nycto <james@commonphp.com>
2009-05-20 08:19:29 -0700

    Added Exception\Interrupt\Soap

---
+++ src/classes/Exception/Interrupt/Soap.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * @license Artistic License 2.0
+ *
+ * This file is part of commonPHP.
+ *
+ * commonPHP is free software: you can redistribute it and/or modify
+ * it under the terms of the Artistic License as published by
+ * the Open Source Initiative, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * commonPHP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Artistic License for more details.
+ *
+ * You should have received a copy of the Artistic License
+ * along with commonPHP. If not, see <http://www.commonphp.com/license.php>
+ * or <http://www.opensource.org/licenses/artistic-license-2.0.php>.
+ *
+ * @author James Frasca <james@commonphp.com>
+ * @copyright Copyright 2008, James Frasca, All Rights Reserved
+ * @package Exception
+ */
+
+namespace cPHP\Exception\Interrupt;
+
+/**
+ * Soap server fault interrupt
+ *
+ * This exception is used by the Soap server class to halt server
+ * execution and return a soap fault
+ */
+class Soap extends \cPHP\Exception\Interrupt
+{
+
+    /**
+     * The title of this exception
+     */
+    const TITLE = "Soap Fault Interruption";
+
+    /**
+     * A brief description of this error type
+     */
+    const DESCRIPTION = "Soap Server Fault Interrupt";
+
+}
+
+?>
