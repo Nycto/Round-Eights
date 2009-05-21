95a30a2f3c1b5dcba444e71aa1d4d072e5d913c7
Nycto <james@commonphp.com>
2009-05-20 20:50:33 -0700

    Implemented basic soap\server functionality

--- src/classes/Soap/Server.php
+++ src/classes/Soap/Server.php
@@ -36,36 +36,166 @@ class Server
      *
      * @var array
      */
-    private $commands = array();
+    private $operations = array();
 
     /**
-     * Returns the list of registered command
+     * The namespace the command elements will be members of
+     *
+     * @var String
+     */
+    private $namespace;
+
+    /**
+     * Constructor...
+     *
+     * @param String $namespace The namespace the command elements will
+     * 		be members of
+     */
+    public function __construct ( $namespace )
+    {
+        $this->namespace = trim( \cPHP\strval( $namespace ) );
+    }
+
+    /**
+     * Returns the list of registered operations
      *
      * @return array Returns an array of \cPHP\iface\Soap\Command objects
      */
-    public function getCommandList ()
+    public function getOperations ()
     {
-        return $this->commands;
+        return $this->operations;
     }
 
     /**
      * Registers a new command
      *
-     * @param String $title The name of the command this object will handle
-     * @param \cPHP\iface\Soap\Command $command The handler to invoke when
+     * @param String $title The name of the operation this object will handle
+     * @param \cPHP\iface\Soap\Command $operation The handler to invoke when
      * 		this command is encountered
      * @return \cPHP\Soap\Server Returns a self reference
      */
-    public function registerCommand ( $title, \cPHP\iface\Soap\Command $command )
+    public function register ( $title, \cPHP\iface\Soap\Command $operation )
     {
         $title = \cPHP\str\stripW( $title );
 
         if ( \cPHP\isEmpty($title) )
             throw new \cPHP\Exception\Argument(0, "Command Title", "Must not be empty");
 
-        $this->commands[ $title ] = $command;
+        $this->operations[ $title ] = $operation;
 
         return $this;
+    }
+
+    /**
+     * Returns the Soap command element from a given DOM Document
+     *
+     * @throws \cPHP\Exception\Interrupt\Soap This is thrown if any error
+     * 		is encountered while parsing the documet
+     * @param DOMDocument $doc The document to parse as a soap request
+     * @return DOMElement The command element
+     */
+    private function getOperationElem ( \DOMDocument $doc )
+    {
+        if ( !$doc->hasChildNodes() ) {
+            throw new \cPHP\Exception\Interrupt\Soap(
+            	"Empty XML Document",
+                1000
+            );
+        }
+
+        $xpath = new \DOMXPath( $doc );
+        $xpath->registerNamespace("soap", "http://www.w3.org/2001/12/soap-envelope");
+        $xpath->registerNamespace("cmd", $this->namespace);
+
+        // Look for the soap envelope
+        if ( $xpath->evaluate("count(/soap:Envelope)") == 0 ) {
+            throw new \cPHP\Exception\Interrupt\Soap(
+            	"Could not find soap envelope",
+                1001
+            );
+        }
+
+        // I couldn't resist this variable name.
+        $bodyCount = $xpath->evaluate("count(/soap:Envelope/soap:Body)");
+
+        // Look for the soap body
+        if ( $bodyCount == 0 ) {
+            throw new \cPHP\Exception\Interrupt\Soap(
+            	"Could not find soap body",
+                1002
+            );
+        }
+
+        // Ensure there aren't multiple soap bodies
+        if ( $bodyCount > 1 ) {
+            throw new \cPHP\Exception\Interrupt\Soap(
+            	"Multiple soap body elements found",
+                1003
+            );
+        }
+
+        $cmdCount = $xpath->evaluate("count(/soap:Envelope/soap:Body/cmd:*)");
+
+        // Look for the soap command tag
+        if ( $cmdCount == 0 ) {
+            throw new \cPHP\Exception\Interrupt\Soap(
+            	"Could not find soap operation element",
+                1004
+            );
+        }
+
+        // Ensure there aren't multiple commands
+        if ( $cmdCount > 1 ) {
+            throw new \cPHP\Exception\Interrupt\Soap(
+            	"Multiple soap operation elements found",
+                1005
+            );
+        }
+
+        // Pull the command node
+        $cmd = $xpath->query("/soap:Envelope/soap:Body/cmd:*");
+
+        return $cmd->item(0);
+    }
+
+    /**
+     * Processes a DOMDocument as an soap request
+     *
+     * In the event of an error, a Soap Fault builder will be returned.
+     *
+     * @param \DOMDocument $doc The document to process
+     * @return \cPHP\iface\XMLBuilder Returns the builder needed to construct
+     * 		the response
+     */
+    public function process ( \DOMDocument $doc )
+    {
+        try {
+
+            // Extract the soap operation element
+            $cmd = $this->getOperationElem( $doc );
+
+            $tag = $cmd->tagName;
+
+            // If the tag is namespaced, just grab the local part
+            if ( \cPHP\str\contains(":", $tag) )
+                $tag = \cPHP\ary\last( explode(":", $tag) );
+
+            if ( !isset($this->operations[ $tag ]) ) {
+                throw new \cPHP\Exception\Interrupt\Soap(
+                        "Invalid soap operation",
+                        1006
+                    );
+            }
+
+            return $this->operations[ $tag ]->getResponseBuilder( $doc, $cmd );
+
+        }
+        catch ( \cPHP\Exception\Interrupt\Soap $err ) {
+            return new \cPHP\XMLBuilder\Soap\Fault(
+                    $err->getCode(),
+                    $err->getMessage()
+                );
+        }
     }
 
 }
--- tests/classes/Soap/Server.php
+++ tests/classes/Soap/Server.php
@@ -35,41 +35,210 @@ class classes_soap_server extends PHPUnit_Framework_TestCase
 
     public function testRegister ()
     {
-        $soap = new \cPHP\Soap\Server;
-        $this->assertSame( array(), $soap->getCommandList() );
+        $soap = new \cPHP\Soap\Server( "uri://example.com" );
+        $this->assertSame( array(), $soap->getOperations() );
 
         $cmd = $this->getMock('\cPHP\iface\Soap\Command');
-        $this->assertSame( $soap, $soap->registerCommand("one", $cmd) );
-        $this->assertSame( array("one" => $cmd), $soap->getCommandList() );
+        $this->assertSame( $soap, $soap->register("one", $cmd) );
+        $this->assertSame( array("one" => $cmd), $soap->getOperations() );
 
         $cmd2 = $this->getMock('\cPHP\iface\Soap\Command');
-        $this->assertSame( $soap, $soap->registerCommand("two", $cmd2) );
+        $this->assertSame( $soap, $soap->register("two", $cmd2) );
         $this->assertSame(
                 array("one" => $cmd, "two" => $cmd2),
-                $soap->getCommandList()
+                $soap->getOperations()
             );
 
-        $this->assertSame( $soap, $soap->registerCommand("one", $cmd2) );
+        $this->assertSame( $soap, $soap->register("one", $cmd2) );
         $this->assertSame(
                 array("one" => $cmd2, "two" => $cmd2),
-                $soap->getCommandList()
+                $soap->getOperations()
             );
     }
 
     public function testRegister_err ()
     {
-        $soap = new \cPHP\Soap\Server;
-        $this->assertSame( array(), $soap->getCommandList() );
+        $soap = new \cPHP\Soap\Server( "uri://example.com" );
+        $this->assertSame( array(), $soap->getOperations() );
 
         $cmd = $this->getMock('\cPHP\iface\Soap\Command');
 
         try {
-            $soap->registerCommand("  ", $cmd);
+            $soap->register("  ", $cmd);
             $this->fail("An expected exception was not thrown");
         }
         catch ( \cPHP\Exception\Argument $err ) {
             $this->assertSame( "Must not be empty", $err->getMessage() );
         }
+    }
+
+    public function testProcess_emptyDoc ()
+    {
+        $soap = new \cPHP\Soap\Server( "uri://example.com" );
+
+        $result = $soap->process( new DOMDocument );
+
+        $this->assertEquals(
+                new \cPHP\XMLBuilder\Soap\Fault(
+                		1000,
+                		"Empty XML Document"
+            		),
+                $result
+            );
+    }
+
+    public function testProcess_noEnvelope ()
+    {
+        $doc = new DOMDocument;
+        $doc->loadXML('<tag />');
+
+        $soap = new \cPHP\Soap\Server( "uri://example.com" );
+        $result = $soap->process( $doc );
+
+        $this->assertEquals(
+                new \cPHP\XMLBuilder\Soap\Fault(
+                		1001,
+                		"Could not find soap envelope"
+            		),
+                $result
+            );
+    }
+
+    public function testProcess_noBody ()
+    {
+        $doc = new DOMDocument;
+        $doc->loadXML('<soap:Envelope xmlns:soap="http://www.w3.org/2001/12/soap-envelope" />');
+
+        $soap = new \cPHP\Soap\Server( "uri://example.com" );
+        $result = $soap->process( $doc );
+
+        $this->assertEquals(
+                new \cPHP\XMLBuilder\Soap\Fault(
+                        1002,
+                        "Could not find soap body"
+                    ),
+                $result
+            );
+    }
+
+    public function testProcess_multiBody ()
+    {
+        $doc = new DOMDocument;
+        $doc->loadXML(
+        	'<soap:Envelope xmlns:soap="http://www.w3.org/2001/12/soap-envelope">'
+        		.'<soap:Body />'
+        		.'<soap:Body />'
+    		.'</soap:Envelope>'
+        );
+
+        $soap = new \cPHP\Soap\Server( "uri://example.com" );
+        $result = $soap->process( $doc );
+
+        $this->assertEquals(
+                new \cPHP\XMLBuilder\Soap\Fault(
+                		1003,
+                		"Multiple soap body elements found"
+                    ),
+                $result
+            );
+    }
+
+    public function testProcess_noCommand ()
+    {
+        $doc = new DOMDocument;
+        $doc->loadXML(
+        	'<soap:Envelope xmlns:soap="http://www.w3.org/2001/12/soap-envelope">'
+        		.'<soap:Body />'
+    		.'</soap:Envelope>'
+        );
+
+        $soap = new \cPHP\Soap\Server( "uri://example.com" );
+        $result = $soap->process( $doc );
+
+        $this->assertEquals(
+                new \cPHP\XMLBuilder\Soap\Fault(
+                		1004,
+                		"Could not find soap operation element"
+                    ),
+                $result
+            );
+    }
+
+    public function testProcess_multiCommand ()
+    {
+        $doc = new DOMDocument;
+        $doc->loadXML(
+        	'<soap:Envelope xmlns:soap="http://www.w3.org/2001/12/soap-envelope">'
+        		.'<soap:Body>'
+        			.'<m:Action xmlns:m="uri://example.com" />'
+        			.'<m:Action2 xmlns:m="uri://example.com" />'
+        		.'</soap:Body>'
+    		.'</soap:Envelope>'
+        );
+
+        $soap = new \cPHP\Soap\Server( "uri://example.com" );
+        $result = $soap->process( $doc );
+
+        $this->assertEquals(
+                new \cPHP\XMLBuilder\Soap\Fault(
+                    	1005,
+                    	"Multiple soap operation elements found"
+                    ),
+                $result
+            );
+    }
+
+    public function testProcess_invalidOperation ()
+    {
+        $doc = new DOMDocument;
+        $doc->loadXML(
+        	'<soap:Envelope xmlns:soap="http://www.w3.org/2001/12/soap-envelope">'
+        		.'<soap:Body>'
+        			.'<m:Action xmlns:m="uri://example.com" />'
+        		.'</soap:Body>'
+    		.'</soap:Envelope>'
+        );
+
+        $soap = new \cPHP\Soap\Server( "uri://example.com" );
+        $result = $soap->process( $doc );
+
+        $this->assertEquals(
+                new \cPHP\XMLBuilder\Soap\Fault(
+                    	1006,
+                    	"Invalid soap operation"
+                    ),
+                $result
+            );
+    }
+
+    public function testProcess_success ()
+    {
+        $doc = new DOMDocument;
+        $doc->loadXML(
+        	'<soap:Envelope xmlns:soap="http://www.w3.org/2001/12/soap-envelope">'
+        		.'<soap:Body>'
+        			.'<m:Action xmlns:m="uri://example.com" />'
+        		.'</soap:Body>'
+    		.'</soap:Envelope>'
+        );
+
+        $soap = new \cPHP\Soap\Server( "uri://example.com" );
+
+        $response = $this->getMock('\cPHP\iface\XMLBuilder');
+
+        $cmd = $this->getMock('\cPHP\iface\Soap\Command');
+        $cmd->expects( $this->once() )
+            ->method( 'getResponseBuilder' )
+            ->with(
+                    $this->equalTo($doc),
+                    $this->isInstanceOf("DOMElement")
+                )
+            ->will( $this->returnValue($response) );
+
+
+        $soap->register( "Action", $cmd );
+
+        $this->assertSame( $response, $soap->process( $doc ) );
     }
 
 }
