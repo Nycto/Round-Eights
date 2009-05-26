05ebeafd65e25c971deb43a25e199822fdf84365
Nycto <james@commonphp.com>
2009-05-26 08:01:40 -0700

    Doc update

--- src/interfaces/MetaDB/Column.php
+++ src/interfaces/MetaDB/Column.php
@@ -20,7 +20,7 @@
  *
  * @author James Frasca <james@commonphp.com>
  * @copyright Copyright 2008, James Frasca, All Rights Reserved
- * @package Soap
+ * @package MetaDB
  */
 
 namespace cPHP\iface\MetaDB;
