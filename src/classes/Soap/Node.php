c4e035260b0f7a1223b38593fd07115cdf0cf819
Nycto <James@RaindropPHP.com>
2009-10-10 19:55:03 -0700

    Added Soap\Node with ->getTag

---
+++ src/classes/Soap/Node.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * @license Artistic License 2.0
+ *
+ * This file is part of RaindropPHP.
+ *
+ * RaindropPHP is free software: you can redistribute it and/or modify
+ * it under the terms of the Artistic License as published by
+ * the Open Source Initiative, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * RaindropPHP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Artistic License for more details.
+ *
+ * You should have received a copy of the Artistic License
+ * along with RaindropPHP. If not, see <http://www.RaindropPHP.com/license.php>
+ * or <http://www.opensource.org/licenses/artistic-license-2.0.php>.
+ *
+ * @author James Frasca <James@RaindropPHP.com>
+ * @copyright Copyright 2008, James Frasca, All Rights Reserved
+ * @package XMLBuilder
+ */
+
+namespace h2o\Soap;
+
+/**
+ * The base class for the Header or Message nodes in a Soap message
+ */
+abstract class Node
+{
+
+    /**
+     * The DOMElement this node represents
+     *
+     * @var DOMElement
+     */
+    private $node;
+
+    /**
+     * Constructor...
+     *
+     * @param DOMElement $node The DOM Element this node represents
+     */
+    public function __construct ( \DOMElement $node )
+    {
+        $this->node = $node;
+    }
+
+    /**
+     * Returns the name of the tag for this node
+     *
+     * @return String
+     */
+    public function getTag ()
+    {
+        return preg_replace( '/.*?:/', '', $this->node->tagName );
+    }
+
+}
+
+?>
