d005ee9b9424644d239c95b0bc48dfdf8ec12893
Nycto <James@RaindropPHP.com>
2009-10-11 10:40:59 -0700

    Added Soap\Node\Header skeleton

---
+++ src/classes/Soap/Node/Header.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @license Artistic License 2.0
+ *
+ * This file is part of RaindropPHP.
+ *
+ * RaindropPHP is free software: you can redistribute it and/or modify
+ * it under the terms of the Artistic License as published by
+ * the Open Source Initiative, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * RaindropPHP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Artistic License for more details.
+ *
+ * You should have received a copy of the Artistic License
+ * along with RaindropPHP. If not, see <http://www.RaindropPHP.com/license.php>
+ * or <http://www.opensource.org/licenses/artistic-license-2.0.php>.
+ *
+ * @author James Frasca <James@RaindropPHP.com>
+ * @copyright Copyright 2008, James Frasca, All Rights Reserved
+ * @package XMLBuilder
+ */
+
+namespace h2o\Soap\Node;
+
+/**
+ * A Soap Header Node
+ */
+class Header extends \h2o\Soap\Node
+{
+
+}
+
+?>
