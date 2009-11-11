086c512de85c315dc36d3470d8b2637e05443297
Nycto <James@RaindropPHP.com>
2009-11-10 21:16:20 -0800

    Added Transform\None

---
+++ tests/classes/Transform/None.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Unit Test File
+ *
+ * @license Artistic License 2.0
+ *
+ * This file is part of RaindropPHP.
+ *
+ * RaindropPHP is free software: you can redistribute it and/or modify
+ * it under the terms of the Artistic License as published by
+ * the Open Source Initiative, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * RaindropPHP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Artistic License for more details.
+ *
+ * You should have received a copy of the Artistic License
+ * along with RaindropPHP. If not, see <http://www.RaindropPHP.com/license.php>
+ * or <http://www.opensource.org/licenses/artistic-license-2.0.php>.
+ *
+ * @author James Frasca <James@RaindropPHP.com>
+ * @copyright Copyright 2008, James Frasca, All Rights Reserved
+ * @package UnitTests
+ */
+
+require_once rtrim( __DIR__, "/" ) ."/../../general.php";
+
+/**
+ * unit tests
+ */
+class classes_Transform_None extends PHPUnit_Framework_TestCase
+{
+
+    public function testTo ()
+    {
+        $encode = new \h2o\Transform\None;
+
+        $this->assertSame(
+                "This is a string",
+                $encode->to("This is a string")
+            );
+    }
+
+    public function testFrom ()
+    {
+        $encode = new \h2o\Transform\None;
+
+        $this->assertSame(
+                "This is a string",
+                $encode->from("This is a string")
+            );
+    }
+
+}
+
+?>
