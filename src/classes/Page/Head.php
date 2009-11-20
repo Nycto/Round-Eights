99cd2545e67ef728389bbd85bc0ad96ec9240723
Nycto <James@RoundEights.com>
2009-11-19 22:45:53 -0800

    Added Page\Head

---
+++ src/classes/Page/Head.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * @license Artistic License 2.0
+ *
+ * This file is part of Round Eights.
+ *
+ * Round Eights is free software: you can redistribute it and/or modify
+ * it under the terms of the Artistic License as published by
+ * the Open Source Initiative, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * Round Eights is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Artistic License for more details.
+ *
+ * You should have received a copy of the Artistic License
+ * along with Round Eights. If not, see <http://www.RoundEights.com/license.php>
+ * or <http://www.opensource.org/licenses/artistic-license-2.0.php>.
+ *
+ * @author James Frasca <James@RoundEights.com>
+ * @copyright Copyright 2008, James Frasca, All Rights Reserved
+ * @package Page
+ */
+
+namespace r8\Page;
+
+/**
+ * Wraps a page in an HTML head
+ */
+class Head implements \r8\iface\Page
+{
+
+    /**
+     * The Source to pull the Head data from
+     *
+     * @var \r8\HTML\Head
+     */
+    private $head;
+
+    /**
+     * The Page being wrapped
+     *
+     * @var \r8\iface\Page
+     */
+    private $page;
+
+    /**
+     * Constructor...
+     *
+     * @param \r8\HTML\Head $head The Source to pull the Head data from
+     * @param \r8\iface\Page $page The page being wrapped
+     */
+    public function __construct ( \r8\HTML\Head $head, \r8\iface\Page $page )
+    {
+        $this->head = $head;
+        $this->page = $page;
+    }
+
+    /**
+     * Returns the core content this page will display
+     *
+     * @param \r8\Page\Context $context A context object which is used by this
+     *      page to communicate with the root page
+     * @return \r8\Template\Blank Returns a blank template
+     */
+    public function getContent ( \r8\Page\Context $context )
+    {
+        $content = $this->page->getContent( $context );
+        $content = $content instanceof \r8\iface\Template
+            ? $content->render() : NULL;
+
+        $docType = $this->head->getDocType()->getValue();
+
+        return new \r8\Template\Raw(
+            ( empty($docType) ? "" : $docType ."\n" )
+            ."<html>\n"
+            .$this->head->getTag() ."\n"
+            ."<body>\n"
+            .$content ."\n"
+            ."</body>\n"
+            ."</html>"
+        );
+    }
+
+}
+
+?>
---
+++ tests/classes/Page/Head.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * Unit Test File
+ *
+ * @license Artistic License 2.0
+ *
+ * This file is part of Round Eights.
+ *
+ * Round Eights is free software: you can redistribute it and/or modify
+ * it under the terms of the Artistic License as published by
+ * the Open Source Initiative, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * Round Eights is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Artistic License for more details.
+ *
+ * You should have received a copy of the Artistic License
+ * along with Round Eights. If not, see <http://www.RoundEights.com/license.php>
+ * or <http://www.opensource.org/licenses/artistic-license-2.0.php>.
+ *
+ * @author James Frasca <James@RoundEights.com>
+ * @copyright Copyright 2008, James Frasca, All Rights Reserved
+ * @package UnitTests
+ */
+
+require_once rtrim( __DIR__, "/" ) ."/../../general.php";
+
+/**
+ * unit tests
+ */
+class classes_Page_Head extends PHPUnit_Framework_TestCase
+{
+
+    public function testGetContent_Empty ()
+    {
+        $context = $this->getMock('\r8\Page\Context');
+
+        $wrapped = $this->getMock('\r8\iface\Page');
+        $wrapped->expects( $this->once() )
+            ->method( "getContent" )
+            ->with( $this->equalTo($context) )
+            ->will( $this->returnValue( new \r8\Template\Blank ) );
+
+        $page = new \r8\Page\Head(
+            new \r8\HTML\Head,
+            $wrapped
+        );
+
+        $result = $page->getContent( $context );
+
+        $this->assertThat( $result, $this->isInstanceOf('\r8\iface\Template') );
+        $this->assertSame(
+            '<html>' ."\n"
+            .'<head></head>' ."\n"
+            .'<body>' ."\n\n"
+            .'</body>' ."\n"
+            .'</html>',
+            $result->render()
+        );
+    }
+
+    public function testGetContent_Full ()
+    {
+        $context = $this->getMock('\r8\Page\Context');
+
+        $wrapped = $this->getMock('\r8\iface\Page');
+        $wrapped->expects( $this->once() )
+            ->method( "getContent" )
+            ->with( $this->equalTo($context) )
+            ->will( $this->returnValue( new \r8\Template\Raw("Page Content") ) );
+
+        $head = new \r8\HTML\Head;
+        $head->setDocType( \r8\HTML\DocType::HTML5() );
+        $head->setTitle("Test");
+        $head->addCSS( new \r8\HTML\CSS("test.css") );
+        $head->addCSS( new \r8\HTML\CSS("example.css") );
+        $head->addJavascript( new \r8\HTML\Javascript("test.js") );
+        $head->addJavascript( new \r8\HTML\Javascript("example.js") );
+        $head->addMetaTag( new \r8\HTML\MetaTag("name", "content") );
+        $head->addMetaTag( new \r8\HTML\MetaTag("robots", "index, follow") );
+
+        $page = new \r8\Page\Head( $head, $wrapped );
+
+        $result = $page->getContent( $context );
+
+        $this->assertThat( $result, $this->isInstanceOf('\r8\iface\Template') );
+        $this->assertSame(
+            '<!DOCTYPE html>' ."\n"
+            .'<html>' ."\n"
+            .'<head>'
+            .'<title>Test</title>' ."\n"
+            .'<meta name="name" content="content" />' ."\n"
+            .'<meta name="robots" content="index, follow" />' ."\n"
+            .'<link rel="stylesheet" href="test.css" type="text/css" media="all" />' ."\n"
+            .'<link rel="stylesheet" href="example.css" type="text/css" media="all" />' ."\n"
+            .'<script type="text/javascript" src="test.js"></script>' ."\n"
+            .'<script type="text/javascript" src="example.js"></script>'
+            .'</head>' ."\n"
+            .'<body>' ."\n"
+            .'Page Content' ."\n"
+            .'</body>' ."\n"
+            .'</html>',
+            $result->render()
+        );
+    }
+
+}
+
+?>
