388ee799725f62239e84cf99d31aefb01899d87d
Nycto <James@RoundEights.com>
2009-11-27 16:54:53 -0800

    Added iface\Backtrace\Formatter

---
+++ src/interfaces/Backtrace/Formatter.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @license Artistic License 2.0
+ *
+ * This file is part of Round Eights.
+ *
+ * Round Eights is free software: you can redistribute it and/or modify
+ * it under the terms of the Artistic License as published by
+ * the Open Source Initiative, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * Round Eights is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Artistic License for more details.
+ *
+ * You should have received a copy of the Artistic License
+ * along with Round Eights. If not, see <http://www.RoundEights.com/license.php>
+ * or <http://www.opensource.org/licenses/artistic-license-2.0.php>.
+ *
+ * @author James Frasca <James@RoundEights.com>
+ * @copyright Copyright 2008, James Frasca, All Rights Reserved
+ * @package Cache
+ */
+
+namespace r8\iface\Backtrace;
+
+/**
+ * The interface for objects that format a backtrace
+ */
+interface Formatter
+{
+
+    /**
+     * Formats a backtrace as a string
+     *
+     * @param \r8\Backtrace $backtrace The backtrace being formatted
+     * @return String
+     */
+    public function format ( \r8\Backtrace $backtrace );
+
+}
+
+?>
