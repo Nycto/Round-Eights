759ed585d249731d33555fd4e80565d3daae936a
Nycto <James@RoundEights.com>
2009-12-13 20:50:20 -0800

    Added UrlSafe flag to Transform\Base64

--- src/classes/Transform/Base64.php
+++ src/classes/Transform/Base64.php
@@ -32,6 +32,26 @@ class Base64 implements \r8\iface\Transform\Encode
 {
 
     /**
+     * Whether to break the specs and use only URL safe characters in
+     * the encoded strings
+     *
+     * @var Boolean
+     */
+    private $urlSafe;
+
+    /**
+     * Constructor...
+     *
+     * @param Boolean $urlSafe Whether to break the specs and use only URL
+     * 		safe characters in the encoded strings. This will also cause the
+     * 		trailing "=" signs to be stripped off
+     */
+    public function __construct ( $urlSafe = FALSE )
+    {
+        $this->urlSafe = (bool) $urlSafe;
+    }
+
+    /**
      * Encodes a string
      *
      * @param mixed $value The value to encode
@@ -39,7 +59,12 @@ class Base64 implements \r8\iface\Transform\Encode
      */
     public function to ( $string )
     {
-        return base64_encode( \r8\strval($string) );
+        $result = base64_encode( \r8\strval($string) );
+
+        if ( $this->urlSafe )
+            $result = \rtrim( \strtr( $result, '+/', '-_' ), "=" );
+
+        return $result;
     }
 
     /**
@@ -50,7 +75,12 @@ class Base64 implements \r8\iface\Transform\Encode
      */
     public function from ( $string )
     {
-        return base64_decode( \r8\strval($string) );
+        $string = \r8\strval($string);
+
+        if ( $this->urlSafe )
+            $string = \strtr( $string, '-_', '+/' );
+
+        return base64_decode( $string );
     }
 
 }
