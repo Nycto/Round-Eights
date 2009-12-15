cbcc9fc70562d8ec4a65c950c4bff30b756fe6b4
Nycto <James@RoundEights.com>
2009-12-15 08:12:42 -0800

    Removed depricated formatting methods from Exception\Argument, switched to using ->getData instead

--- tests/classes/Exception/Argument.php
+++ tests/classes/Exception/Argument.php
@@ -33,37 +33,42 @@ require_once rtrim( __DIR__, "/" ) ."/../../general.php";
 class classes_exception_argument extends PHPUnit_Framework_TestCase
 {
 
-    // Returns an thrown exception
-    public function getThrown ()
+    /**
+     * Returns a test exception
+     *
+     * @return \r8\Exception\Argument
+     */
+    public function getTestException ()
     {
-        try {
-            $throw = function ( $arg1, $arg2 ) {
-                throw new \r8\Exception\Argument(0, "test", "From our sponsors", 505, 0);
-            };
-            $throw("arg value", "other arg");
-        }
-        catch ( \r8\Exception\Argument $err ) {
-            return $err;
-        }
+        $throw = function ( $arg1, $arg2 ) {
+            return new \r8\Exception\Argument(0, "test", "From our sponsors", 505, 0);
+        };
+        return $throw("arg value", "other arg");
     }
 
     public function testConstruct ()
     {
-        $err = $this->getThrown();
+        $err = $this->getTestException();
 
         $this->assertEquals( 0, $err->getArgOffset() );
         $this->assertEquals( "From our sponsors", $err->getMessage() );
         $this->assertEquals( 505, $err->getCode() );
 
-        $this->assertSame( array("Arg Label" => "test"), $err->getData() );
+        $this->assertSame(
+            array(
+                'Arg Offset' => 0,
+                'Arg Value' => "string('arg value')",
+                'Arg Label' => 'test',
+            ),
+            $err->getData()
+        );
 
         $this->assertEquals( 0, $err->getFaultOffset() );
-
     }
 
     public function testArg ()
     {
-        $err = $this->getThrown();
+        $err = $this->getTestException();
 
         $this->assertTrue( $err->issetArg() );
         $this->assertEquals( 0, $err->getArgOffset() );
@@ -77,7 +82,7 @@ class classes_exception_argument extends PHPUnit_Framework_TestCase
 
     public function testUnsetArg ()
     {
-        $err = $this->getThrown();
+        $err = $this->getTestException();
 
         $this->assertTrue( $err->issetArg() );
 
@@ -90,7 +95,7 @@ class classes_exception_argument extends PHPUnit_Framework_TestCase
 
     public function testFaultChange ()
     {
-        $err = $this->getThrown("garbage arg", "arg v2", "yet another");
+        $err = $this->getTestException("garbage arg", "arg v2", "yet another");
 
         $this->assertEquals( "arg value", $err->getArgData() );
 
@@ -102,7 +107,7 @@ class classes_exception_argument extends PHPUnit_Framework_TestCase
 
     public function testNoFault ()
     {
-        $err = $this->getThrown();
+        $err = $this->getTestException();
 
         $this->assertTrue( $err->issetFault() );
         $this->assertTrue( $err->issetArg() );
@@ -124,7 +129,7 @@ class classes_exception_argument extends PHPUnit_Framework_TestCase
 
     public function testNoArgs ()
     {
-        $err = $this->getThrown();
+        $err = $this->getTestException();
 
         // Shift the fault to a function call that doesn't have any arguments
         $err->shiftFault();
@@ -132,24 +137,6 @@ class classes_exception_argument extends PHPUnit_Framework_TestCase
         $this->assertFalse( $err->issetArg() );
         $this->assertFalse( $err->getArgOffset() );
         $this->assertNull( $err->getArgData() );
-    }
-
-    public function testDetailsString ()
-    {
-        $err = $this->getThrown();
-
-        $this->assertContains("Arg Offset: 0", $err->getDetailsString());
-        $this->assertContains("Arg Value: string('arg value')", $err->getDetailsString());
-        $this->assertContains("Arg Label: test", $err->getDetailsString());
-    }
-
-    public function testDetailsHTML ()
-    {
-        $err = $this->getThrown();
-
-        $this->assertContains("<dt>Arg Offset</dt><dd>0</dd>", $err->getDetailsHTML());
-        $this->assertContains("<dt>Arg Value</dt><dd>string('arg value')</dd>", $err->getDetailsHTML());
-        $this->assertContains("<dt>Arg Label</dt><dd>test</dd>", $err->getDetailsHTML());
     }
 
 }
