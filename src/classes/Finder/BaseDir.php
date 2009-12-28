42052b445ec4f25fff787c1b0b94ced4fef53635
Nycto <James@RoundEights.com>
2009-12-27 23:02:07 -0800

    Added Finder\BaseDir

---
+++ src/classes/Finder/BaseDir.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * @license Artistic License 2.0
+ *
+ * This file is part of Round Eights.
+ *
+ * Round Eights is free software: you can redistribute it and/or modify
+ * it under the terms of the Artistic License as published by
+ * the Open Source Initiative, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * Round Eights is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Artistic License for more details.
+ *
+ * You should have received a copy of the Artistic License
+ * along with Round Eights. If not, see <http://www.RoundEights.com/license.php>
+ * or <http://www.opensource.org/licenses/artistic-license-2.0.php>.
+ *
+ * @author James Frasca <James@RoundEights.com>
+ * @copyright Copyright 2009, James Frasca, All Rights Reserved
+ * @package Finder
+ */
+
+namespace r8\Finder;
+
+/**
+ * Swaps out the base directory
+ */
+class BaseDir implements \r8\iface\Finder
+{
+
+    /**
+     * The finder being wrapped
+     *
+     * @var \r8\iface\Finder
+     */
+    private $wrapped;
+
+    /**
+     * The list of directories to test
+     *
+     * @var Array
+     */
+    private $dirs = array();
+
+    /**
+     * Constructor...
+     *
+     * @param \r8\iface\Finder $wrapped The finder being wrapped
+     * @param String $dirs... Sub-directories to prepend during process
+     */
+    public function __construct ( \r8\iface\Finder $wrapped )
+    {
+        $this->wrapped = $wrapped;
+
+        if ( func_num_args() > 1 ) {
+            $args = func_get_args();
+            array_shift( $args );
+            array_map( array($this, "addDir"), $args );
+        }
+    }
+
+    /**
+     * Returns the list of sub-directories registered in this instance
+     *
+     * @return Array An array of strings
+     */
+    public function getDirs ()
+    {
+        return $this->dirs;
+    }
+
+    /**
+     * Adds a new sub-directory to the list that will be prepended
+     *
+     * @param String $subdir
+     * @return \r8\Finder\BaseDir Returns a self reference
+     */
+    public function addDir ( $subdir )
+    {
+        $subdir = rtrim( (string) $subdir, "/" );
+
+        if ( !\r8\IsEmpty($subdir) )
+            $this->dirs[] = $subdir;
+
+        return $this;
+    }
+
+    /**
+     * Attempts to find a file given a relative path
+     *
+     * @param \r8\Finder\Tracker $tracker $file The tracker to use when determining
+     *      if a base/path combination is valid
+     * @param String $base The base directory to look for the path in
+     * @param String $path The path being looked for
+     * @return Boolean Returns whether the path was found
+     */
+    public function find ( \r8\Finder\Tracker $tracker, $base, $path )
+    {
+        foreach ( $this->dirs AS $inc ) {
+            if ( $this->wrapped->find($tracker, $inc, $path) )
+                return TRUE;
+        }
+
+        return FALSE;
+    }
+
+}
+
+?>
---
+++ tests/classes/Finder/BaseDir.php
@@ -0,0 +1,132 @@
+<?php
+/**
+ * Unit Test File
+ *
+ * @license Artistic License 2.0
+ *
+ * This file is part of Round Eights.
+ *
+ * Round Eights is free software: you can redistribute it and/or modify
+ * it under the terms of the Artistic License as published by
+ * the Open Source Initiative, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * Round Eights is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Artistic License for more details.
+ *
+ * You should have received a copy of the Artistic License
+ * along with Round Eights. If not, see <http://www.RoundEights.com/license.php>
+ * or <http://www.opensource.org/licenses/artistic-license-2.0.php>.
+ *
+ * @author James Frasca <James@RoundEights.com>
+ * @copyright Copyright 2009, James Frasca, All Rights Reserved
+ * @package UnitTests
+ */
+
+require_once rtrim( __DIR__, "/" ) ."/../../general.php";
+
+/**
+ * unit tests
+ */
+class classes_Finder_BaseDir extends PHPUnit_Framework_TestCase
+{
+
+    public function testAddDir ()
+    {
+        $wrapped = $this->getMock('\r8\iface\Finder');
+
+        $dir = new \r8\Finder\BaseDir( $wrapped );
+        $this->assertSame( array(), $dir->getDirs() );
+
+        $this->assertSame( $dir, $dir->addDir("subdir") );
+        $this->assertSame( array("subdir"), $dir->getDirs() );
+
+        $this->assertSame( $dir, $dir->addDir("/dir/path/") );
+        $this->assertSame( array("subdir", "/dir/path"), $dir->getDirs() );
+    }
+
+    public function testConstruct ()
+    {
+        $wrapped = $this->getMock('\r8\iface\Finder');
+        $dir = new \r8\Finder\BaseDir( $wrapped, "subdir", "/dir/path/" );
+        $this->assertSame( array("subdir", "/dir/path"), $dir->getDirs() );
+    }
+
+    public function testFind_FirstDir ()
+    {
+        $tracker = $this->getMock('\r8\Finder\Tracker');
+
+        $wrapped = $this->getMock('\r8\iface\Finder');
+        $wrapped->expects( $this->once() )
+            ->method( "find" )
+            ->with(
+                $this->equalTo($tracker),
+                $this->equalTo("first"),
+                $this->equalTo("file")
+            )
+            ->will( $this->returnValue( TRUE ) );
+
+        $dir = new \r8\Finder\BaseDir( $wrapped, "first", "second" );
+
+        $this->assertTrue( $dir->find( $tracker, "base", "file" ) );
+    }
+
+    public function testFind_SecondDir ()
+    {
+        $tracker = $this->getMock('\r8\Finder\Tracker');
+
+        $wrapped = $this->getMock('\r8\iface\Finder');
+        $wrapped->expects( $this->at(0) )
+            ->method( "find" )
+            ->with(
+                $this->equalTo($tracker),
+                $this->equalTo("first"),
+                $this->equalTo("file")
+            )
+            ->will( $this->returnValue( FALSE ) );
+        $wrapped->expects( $this->at(1) )
+            ->method( "find" )
+            ->with(
+                $this->equalTo($tracker),
+                $this->equalTo("second"),
+                $this->equalTo("file")
+            )
+            ->will( $this->returnValue( TRUE ) );
+
+        $dir = new \r8\Finder\BaseDir( $wrapped, "first", "second" );
+
+        $this->assertTrue( $dir->find( $tracker, "base", "file" ) );
+    }
+
+    public function testFind_Unfound ()
+    {
+        $tracker = $this->getMock('\r8\Finder\Tracker');
+
+        $wrapped = $this->getMock('\r8\iface\Finder');
+        $wrapped->expects( $this->at(0) )
+            ->method( "find" )
+            ->with(
+                $this->equalTo($tracker),
+                $this->equalTo("first"),
+                $this->equalTo("file")
+            )
+            ->will( $this->returnValue( FALSE ) );
+        $wrapped->expects( $this->at(1) )
+            ->method( "find" )
+            ->with(
+                $this->equalTo($tracker),
+                $this->equalTo("second"),
+                $this->equalTo("file")
+            )
+            ->will( $this->returnValue( FALSE ) );
+
+        $dir = new \r8\Finder\BaseDir( $wrapped, "first", "second" );
+
+        $this->assertFalse( $dir->find( $tracker, "base", "file" ) );
+    }
+
+}
+
+?>
