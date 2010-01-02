1c82d0babe870c39fce1c706c3a744548dcb6b5b
Nycto <James@RoundEights.com>
2010-01-02 13:55:40 -0800

    Added Benchmark/Suite

---
+++ src/classes/Benchmark/Suite.php
@@ -0,0 +1,168 @@
+<?php
+/**
+ * @license Artistic License 2.0
+ *
+ * This file is part of Round Eights.
+ *
+ * Round Eights is free software: you can redistribute it and/or modify
+ * it under the terms of the Artistic License as published by
+ * the Open Source Initiative, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * Round Eights is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Artistic License for more details.
+ *
+ * You should have received a copy of the Artistic License
+ * along with Round Eights. If not, see <http://www.RoundEights.com/license.php>
+ * or <http://www.opensource.org/licenses/artistic-license-2.0.php>.
+ *
+ * @author James Frasca <James@RoundEights.com>
+ * @copyright Copyright 2009, James Frasca, All Rights Reserved
+ * @package Benchmark
+ */
+
+namespace r8\Benchmark;
+
+/**
+ * A suite of benchmarks
+ */
+class Suite
+{
+
+    /**
+     * The number of times to run each test
+     *
+     * @var Integer
+     */
+    private $times;
+
+    /**
+     * The list of benchmarks in this Suite
+     *
+     * @var Array
+     */
+    private $benchmarks = array();
+
+    /**
+     * Constructor...
+     *
+     * @param Integer $times The number of times to run each test
+     */
+    public function __construct ( $times = 1000 )
+    {
+        $this->times = max(1, (int) $times);
+    }
+
+    /**
+     * Returns the Benchmarks loaded in this suite
+     *
+     * @return Array An array of \r8\Benchmark objects
+     */
+    public function getBenchmarks ()
+    {
+        return $this->benchmarks;
+    }
+
+    /**
+     * Adds a new test to this instance
+     *
+     * @param \r8\Benchmark $benchmark The benchmark to add to this suite
+     * @return \r8\Benchmark\Suite
+     */
+    public function addBenchmark ( \r8\Benchmark $benchmark )
+    {
+        if ( !in_array($benchmark, $this->benchmarks, TRUE) )
+            $this->benchmarks[] = $benchmark;
+        return $this;
+    }
+
+    /**
+     * Runs the suite of tests and returns the array of results
+     *
+     * @return Array An array of \r8\Benchmark\Result object
+     */
+    public function run ()
+    {
+        $result = array();
+
+        foreach ( $this->benchmarks AS $benchmark ) {
+            $result[] = $benchmark->run( $this->times );
+        }
+
+        return $result;
+    }
+
+    /**
+     * Dumps the results of this test suite to the client formatted for the command line
+     *
+     * @return \r8\Benchmark\Suite
+     */
+    public function dumpCLI ()
+    {
+        echo "Benchmark Suite Results:\n";
+
+        $resultList = $this->run();
+
+        if ( count($resultList) <= 0 )
+            echo "    No tests found\n";
+
+        foreach ( $resultList AS $result ) {
+            echo "    Test: ". $result->getName() ."\n"
+                ."        Total Time: ". $result->getTotalTime() ." seconds\n"
+                ."        Iterations: ". $result->count() ."\n"
+                ."        Average Time: ". $result->getAverageTime() ." seconds\n";
+        }
+
+        return $this;
+    }
+
+    /**
+     * Dumps the results of this test suite to the client formatted as HTML
+     *
+     * @return \r8\Benchmark\Suite
+     */
+    public function dumpHTML ()
+    {
+        echo "<table>\n"
+            ."<tr><th colspan='4'>Benchmark Suite Results</th></tr>\n"
+            ."<tr>\n"
+                ."<th>Test Name</th>\n"
+                ."<th>Total Time</th>\n"
+                ."<th>Iterations</th>\n"
+                ."<th>Average Time</th>\n"
+            ."</tr>\n";
+
+        $resultList = $this->run();
+
+        if ( count($resultList) <= 0 )
+            echo "<tr><td colspan='4'>No tests found</td></tr>\n";
+
+        foreach ( $resultList AS $result ) {
+            echo "<tr>\n"
+                ."<td>". htmlspecialchars( $result->getName() ) ."</td>\n"
+                ."<td>". htmlspecialchars( $result->getTotalTime() ) ." seconds</td>\n"
+                ."<td>". htmlspecialchars( $result->count() ) ."</td>\n"
+                ."<td>". htmlspecialchars( $result->getAverageTime() ) ." seconds</td>\n"
+                ."</tr>\n";
+        }
+
+        echo "</table>\n";
+
+        return $this;
+    }
+
+    /**
+     * Dumps the results of this test suite to the client
+     *
+     * @return \r8\Benchmark\Suite
+     */
+    public function dump ()
+    {
+        return \r8\Env::Request()->isCLI() ? $this->dumpCLI() : $this->dumpHTML();
+    }
+
+}
+
+?>
---
+++ tests/classes/Benchmark/Suite.php
@@ -0,0 +1,162 @@
+<?php
+/**
+ * Unit Test File
+ *
+ * @license Artistic License 2.0
+ *
+ * This file is part of Round Eights.
+ *
+ * Round Eights is free software: you can redistribute it and/or modify
+ * it under the terms of the Artistic License as published by
+ * the Open Source Initiative, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * Round Eights is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Artistic License for more details.
+ *
+ * You should have received a copy of the Artistic License
+ * along with Round Eights. If not, see <http://www.RoundEights.com/license.php>
+ * or <http://www.opensource.org/licenses/artistic-license-2.0.php>.
+ *
+ * @author James Frasca <James@RoundEights.com>
+ * @copyright Copyright 2009, James Frasca, All Rights Reserved
+ * @package UnitTests
+ */
+
+require_once rtrim( __DIR__, "/" ) ."/../../general.php";
+
+/**
+ * unit tests
+ */
+class classes_Benchmark_Suite extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Returns a test benchmark
+     *
+     * @return \r8\Benchmark
+     */
+    public function getTestBenchmark ( $times = 1000, \r8\Benchmark\Result $result = null )
+    {
+        $mark = $this->getMock('\r8\Benchmark', array(), array(), '', FALSE);
+
+        if ( $result ) {
+            $mark->expects( $this->once() )
+                ->method( "run" )
+                ->with( $this->equalTo( $times ) )
+                ->will( $this->returnValue( $result ) );
+        }
+
+        return $mark;
+    }
+
+    /**
+     * Returns a test benchmark result
+     *
+     * @return \r8\Benchmark\Result
+     */
+    public function getTestResult ( $name = "test", $time = 5, $count = 1000 )
+    {
+        $result = $this->getMock(
+            '\r8\Benchmark\Result',
+            array("getTotalTime", "count"),
+            array($name)
+        );
+
+        $result->expects( $this->any() )
+            ->method( "getTotalTime" )
+            ->will( $this->returnValue( $time ) );
+
+        $result->expects( $this->any() )
+            ->method( "count" )
+            ->will( $this->returnValue( $count ) );
+
+        return $result;
+    }
+
+    public function testAddBenchmark ()
+    {
+        $suite = new \r8\Benchmark\Suite;
+        $this->assertSame( array(), $suite->getBenchmarks() );
+
+        $mark1 = $this->getTestBenchmark();
+        $this->assertSame( $suite, $suite->addBenchmark($mark1) );
+        $this->assertSame( array($mark1), $suite->getBenchmarks() );
+
+        $mark2 = $this->getTestBenchmark();
+        $this->assertSame( $suite, $suite->addBenchmark($mark2) );
+        $this->assertSame( array($mark1, $mark2), $suite->getBenchmarks() );
+
+        $this->assertSame( $suite, $suite->addBenchmark($mark1) );
+        $this->assertSame( array($mark1, $mark2), $suite->getBenchmarks() );
+    }
+
+    public function testRun_Empty ()
+    {
+        $suite = new \r8\Benchmark\Suite;
+        $this->assertSame( array(), $suite->run() );
+    }
+
+    public function testRun_Full ()
+    {
+        $suite = new \r8\Benchmark\Suite( 100 );
+
+        $result1 = $this->getTestResult();
+        $suite->addBenchmark( $this->getTestBenchmark( 100, $result1 ) );
+
+        $result2 = $this->getTestResult();
+        $suite->addBenchmark( $this->getTestBenchmark( 100, $result2 ) );
+
+        $this->assertSame( array($result1, $result2), $suite->run() );
+    }
+
+    public function testDumpCLI ()
+    {
+        $suite = new \r8\Benchmark\Suite( 100 );
+        $suite->addBenchmark(
+            $this->getTestBenchmark( 100, $this->getTestResult( "Test One", 5, 100) )
+        );
+        $suite->addBenchmark(
+            $this->getTestBenchmark( 100, $this->getTestResult( "Test 2", 5, 1000) )
+        );
+
+        ob_start();
+        $this->assertSame( $suite, $suite->dumpCLI() );
+        $this->assertGreaterThan( 0, strlen(ob_get_clean()) );
+    }
+
+    public function testDumpHTML ()
+    {
+        $suite = new \r8\Benchmark\Suite( 100 );
+        $suite->addBenchmark(
+            $this->getTestBenchmark( 100, $this->getTestResult( "Test One", 5, 100) )
+        );
+        $suite->addBenchmark(
+            $this->getTestBenchmark( 100, $this->getTestResult( "Test 2", 5, 1000) )
+        );
+
+        ob_start();
+        $this->assertSame( $suite, $suite->dumpHTML() );
+        $this->assertGreaterThan( 0, strlen(ob_get_clean()) );
+    }
+
+    public function testDump ()
+    {
+        $suite = new \r8\Benchmark\Suite( 100 );
+        $suite->addBenchmark(
+            $this->getTestBenchmark( 100, $this->getTestResult( "Test One", 5, 100) )
+        );
+        $suite->addBenchmark(
+            $this->getTestBenchmark( 100, $this->getTestResult( "Test 2", 5, 1000) )
+        );
+
+        ob_start();
+        $this->assertSame( $suite, $suite->dump() );
+        $this->assertGreaterThan( 0, strlen(ob_get_clean()) );
+    }
+
+}
+
+?>
