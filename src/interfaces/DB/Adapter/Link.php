a537d1803d46fc65f07d7e960c563f3d42daec0f
James <James@RoundEights.com>
2010-01-13 13:00:28 -0800

    Added DB\Adapter\Link and DB\Adapter\Result

---
+++ src/interfaces/DB/Adapter/Link.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * @license Artistic License 2.0
+ *
+ * This file is part of Round Eights.
+ *
+ * Round Eights is free software: you can redistribute it and/or modify
+ * it under the terms of the Artistic License as published by
+ * the Open Source Initiative, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * Round Eights is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Artistic License for more details.
+ *
+ * You should have received a copy of the Artistic License
+ * along with Round Eights. If not, see <http://www.RoundEights.com/license.php>
+ * or <http://www.opensource.org/licenses/artistic-license-2.0.php>.
+ *
+ * @author James Frasca <James@RoundEights.com>
+ * @copyright Copyright 2009, James Frasca, All Rights Reserved
+ * @package Database
+ */
+
+namespace r8\iface\DB\Adapter;
+
+/**
+ * Database Connection Adapter
+ */
+interface Link
+{
+
+    /**
+     * Connect to the server
+     *
+     * @return Resource Returns a database connection resource
+     */
+    public function connect ();
+
+    /**
+     * Given a string, escapes it for use in a query
+     *
+     * @return String Returns the escaped string
+     */
+    public function escape ( $string );
+
+    /**
+     * Execute a query and return a result object
+     *
+     * @param String $query The query to execute
+     * @return \r8\iface\DB\Adapter\Result
+     */
+    public function query ( $query );
+
+    /**
+     * Disconnect from the server
+     *
+     * @return null
+     */
+    public function disconnect ();
+
+    /**
+     * Returns whether this connection is active
+     *
+     * @return Boolean
+     */
+    public function isConnected ();
+}
+
+?>
---
+++ src/interfaces/DB/Adapter/Result.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Database Query Result
+ *
+ * @license Artistic License 2.0
+ *
+ * This file is part of Round Eights.
+ *
+ * Round Eights is free software: you can redistribute it and/or modify
+ * it under the terms of the Artistic License as published by
+ * the Open Source Initiative, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * Round Eights is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Artistic License for more details.
+ *
+ * You should have received a copy of the Artistic License
+ * along with Round Eights. If not, see <http://www.RoundEights.com/license.php>
+ * or <http://www.opensource.org/licenses/artistic-license-2.0.php>.
+ *
+ * @author James Frasca <James@RoundEights.com>
+ * @copyright Copyright 2009, James Frasca, All Rights Reserved
+ * @package Database
+ */
+
+namespace r8\iface\DB\Adapter;
+
+/**
+ * Database Result adapter
+ */
+interface Result extends \Countable
+{
+
+    /**
+     * Internal method that returns the number of rows found
+     *
+     * @return Integer
+     */
+    public function count ();
+
+    /**
+     * Internal method to get a list of field names returned
+     *
+     * @return Integer
+     */
+    public function getFields ();
+
+    /**
+     * Internal method to fetch the next row in a result set
+     *
+     * @return Array Returns the field values
+     */
+    public function fetch ();
+
+    /**
+     * Internal method to seek to a specific row in a result resource
+     *
+     * @param Integer $offset The offset to seek to
+     * @return Array Returns the field values
+     */
+    public function seek ($offset);
+    
+    /**
+     * Returns the number of rows affected by this query
+     *
+     * @return Integer
+     */
+    public function getAffected ();
+    
+    /**
+     * Returns the Insert ID 
+     *
+     * @return Integer
+     */
+    public function getInsertID ();
+
+    /**
+     * Internal method to free the result resource
+     *
+     * @return null
+     */
+    public function free ();
+
+}
+
+?>
