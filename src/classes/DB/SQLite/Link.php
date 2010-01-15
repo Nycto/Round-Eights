7c95932fe15b3eb3d18c09f54831b892eb9191b9
James <James@RoundEights.com>
2010-01-15 12:54:40 -0800

    Added DB\SQLite\Link

---
+++ src/classes/DB/SQLite/Link.php
@@ -0,0 +1,184 @@
+<?php
+/**
+ * @license Artistic License 2.0
+ *
+ * This file is part of Round Eights.
+ *
+ * Round Eights is free software: you can redistribute it and/or modify
+ * it under the terms of the Artistic License as published by
+ * the Open Source Initiative, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * Round Eights is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Artistic License for more details.
+ *
+ * You should have received a copy of the Artistic License
+ * along with Round Eights. If not, see <http://www.RoundEights.com/license.php>
+ * or <http://www.opensource.org/licenses/artistic-license-2.0.php>.
+ *
+ * @author James Frasca <James@RoundEights.com>
+ * @copyright Copyright 2009, James Frasca, All Rights Reserved
+ * @package Database
+ */
+
+namespace r8\DB\SQLite;
+
+/**
+ * SQLite Database Link
+ */
+class Link implements \r8\iface\DB\Adapter\Link
+{
+
+    /**
+     * The SQLite database file to use
+     *
+     * @var String
+     */
+    private $file;
+
+    /**
+     * The configuration options for this connection
+     *
+     * @var \r8\DB\Config
+     */
+    private $config;
+
+    /**
+     * The database connection this link represents
+     *
+     * @var Resource
+     */
+    private $link;
+
+    /**
+     * Constructor...
+     *
+     * @param String $file The SQLite database file to use
+     * @param \r8\DB\Config $config The credentials to use for opening the connection
+     */
+    public function __construct ( $file, \r8\DB\Config $config )
+    {
+        $file = trim( (string) $file );
+        if ( empty($file) )
+            throw new \r8\Exception\Argument(0, "Database File", "Must not be empty");
+
+        $this->file = $file;
+        $this->config = $config;
+    }
+
+    /**
+     * Returns whether this connection is active
+     *
+     * @return Boolean
+     */
+    public function isConnected ()
+    {
+        return isset($this->link) ? TRUE : FALSE;
+    }
+
+    /**
+     * Connect to the server
+     *
+     * @return NULL
+     */
+    public function connect ()
+    {
+        if ( isset($this->link) )
+            return NULL;
+
+        $error = null;
+
+        $link = @sqlite_open( $this->file, 0666, $error );
+
+        if ( !$link ) {
+            throw new \r8\Exception\DB\Link( $error, NULL, $this );
+        }
+
+        $this->link = $link;
+    }
+
+    /**
+     * Given a string, escapes it for use in a query
+     *
+     * @param String $value The string to escape
+     * @return String Returns the escaped string
+     */
+    public function escape ( $value )
+    {
+        return sqlite_escape_string( (string) $value );
+    }
+
+    /**
+     * Execute a query and return a result object
+     *
+     * @param String $query The query to execute
+     * @return \r8\iface\DB\Adapter\Result
+     */
+    public function query ( $query )
+    {
+        if ( !isset($this->link) )
+            $this->connect();
+
+        $error = null;
+        $result = @sqlite_query( $this->link, $query, SQLITE_ASSOC, $error );
+
+        if ( $result === FALSE ) {
+            $code = sqlite_last_error( $this->link );
+            throw new \r8\Exception\DB\Query(
+                $query,
+                sqlite_error_string($code) .": ". $error,
+                $code
+            );
+        }
+
+        if ( \r8\DB\Link::isSelect($query) ) {
+            return new \r8\DB\Result\Read(
+                new \r8\DB\SQLite\Result( $result ),
+                $query
+            );
+        }
+        else {
+            return new \r8\DB\Result\Write(
+                sqlite_changes( $this->link ),
+                sqlite_last_insert_rowid( $this->link ),
+                $query
+            );
+        }
+    }
+
+    /**
+     * Disconnect from the server
+     *
+     * @return null
+     */
+    public function disconnect ()
+    {
+        sqlite_close( $this->link );
+        $this->link = null;
+    }
+
+    /**
+     * Returns the name of the extension required to utilize this link
+     *
+     * @return String|NULL Returns NULL if no specific extension is required
+     */
+    public function getExtension ()
+    {
+        return "sqlite";
+    }
+
+    /**
+     * Returns a brief string that can be used to describe this connection
+     *
+     * @return String Returns a URI that loosely identifies this connection
+     */
+    public function getIdentifier ()
+    {
+        return "sqlite://". $this->file;
+    }
+
+}
+
+?>
---
+++ tests/classes/DB/SQLite/Link.php
@@ -0,0 +1,192 @@
+<?php
+/**
+ * Unit Test File
+ *
+ * @license Artistic License 2.0
+ *
+ * This file is part of Round Eights.
+ *
+ * Round Eights is free software: you can redistribute it and/or modify
+ * it under the terms of the Artistic License as published by
+ * the Open Source Initiative, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * Round Eights is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Artistic License for more details.
+ *
+ * You should have received a copy of the Artistic License
+ * along with Round Eights. If not, see <http://www.RoundEights.com/license.php>
+ * or <http://www.opensource.org/licenses/artistic-license-2.0.php>.
+ *
+ * @author James Frasca <James@RoundEights.com>
+ * @copyright Copyright 2009, James Frasca, All Rights Reserved
+ * @package UnitTests
+ */
+
+require_once rtrim( __DIR__, "/" ) ."/../../../general.php";
+
+/**
+ * unit tests
+ */
+class classes_DB_SQLite_Link extends PHPUnit_Framework_TestCase
+{
+
+	/**
+     * Ensures that all the prerequisites exist for connecting to a SQLite db
+     */
+    public function setUp ()
+    {
+        if ( !extension_loaded("sqlite") )
+            $this->markTestSkipped("SQLite extension is not loaded");
+
+        // Ensure the proper configuration exists
+        $config = new r8_Test_Config( "SQLITE", array("FILE", "TABLE") );
+        $config->test();
+
+        // Test the connection
+        $error = null;
+        $db = @sqlite_open( SQLITE_FILE, 0666, $error );
+        if ( !$db )
+            $this->markTestSkipped("SQLite Connection Error: ". $error);
+	}
+
+	/**
+	 * Returns a test database connection
+	 *
+	 * @return \r8\DB\SQLite\Link
+	 */
+	public function getTestLink ()
+	{
+		return new \r8\DB\SQLite\Link(
+            SQLITE_FILE,
+			new \r8\DB\Config
+		);
+	}
+
+    public function testConnection_error ()
+    {
+        $link = new \r8\DB\SQLite\Link(
+            "/tmp",
+			new \r8\DB\Config
+		);
+
+        try {
+            $link->connect();
+            $this->fail("An expected exception was not thrown");
+        }
+        catch ( \r8\Exception\DB\Link $err ) {
+            $this->assertContains(
+                    "unable to open database: /tmp",
+                    $err->getMessage()
+                );
+        }
+    }
+
+    public function testConnection ()
+    {
+		$link = $this->getTestLink();
+        $this->assertFalse( $link->isConnected() );
+        $this->assertNull( $link->connect() );
+        $this->assertTrue( $link->isConnected() );
+    }
+
+    public function testEscapeString ()
+    {
+		$link = $this->getTestLink();
+        $this->assertSame(
+			"This ''is'' a string",
+			$link->escape("This 'is' a string")
+		);
+    }
+
+    public function testQuery_read ()
+    {
+		$link = $this->getTestLink();
+
+        $result = $link->query("SELECT 50 + 10 AS result");
+
+        $this->assertThat( $result, $this->isInstanceOf('\r8\DB\Result\Read') );
+
+		PHPUnit_Framework_Constraint_SQL::assert(
+			"SELECT 50 + 10 AS result",
+			$result->getQuery()
+		);
+
+        $this->assertSame( 1, $result->count() );
+        $this->assertSame( array("result"), $result->getFields() );
+        $this->assertEquals( array("result" => 60), $result->current() );
+    }
+
+    public function testQuery_write ()
+    {
+		$link = $this->getTestLink();
+
+        $result = $link->query(
+			"CREATE TEMPORARY TABLE ". SQLITE_TABLE ."(id INT, str TEXT)"
+		);
+
+        $this->assertThat( $result, $this->isInstanceOf('\r8\DB\Result\Write') );
+		$this->assertSame( 0, $result->getAffected() );
+		$this->assertNull( $result->getInsertID() );
+
+
+        $result = $link->query("INSERT INTO ". SQLITE_TABLE ." (str) VALUES ('alpha')");
+
+        $this->assertThat( $result, $this->isInstanceOf('\r8\DB\Result\Write') );
+		$this->assertSame( 1, $result->getAffected() );
+		$this->assertSame( 1, $result->getInsertID() );
+
+		PHPUnit_Framework_Constraint_SQL::assert(
+			"INSERT INTO ". SQLITE_TABLE ." (str) VALUES ('alpha')",
+			$result->getQuery()
+		);
+    }
+
+    public function testQuery_Error ()
+    {
+		$link = $this->getTestLink();
+
+		try {
+			$link->query("Not a valid query");
+			$this->fail("An expected exception was not thrown");
+		}
+		catch ( \r8\Exception\DB\Query $err ) {
+			$this->assertContains( "SQL logic error or missing database", $err->getMessage() );
+		}
+    }
+
+    public function testDisconnect ()
+    {
+		$link = $this->getTestLink();
+
+		$link->connect();
+        $this->assertTrue( $link->isConnected() );
+
+        $this->assertNull( $link->disconnect() );
+        $this->assertFalse( $link->isConnected() );
+    }
+
+    public function testGetIdentifier ()
+    {
+        $link = new \r8\DB\SQLite\Link(
+            "/path/to/database.sqlite",
+			new \r8\DB\Config
+		);
+
+        $this->assertSame(
+                "sqlite:///path/to/database.sqlite",
+                $link->getIdentifier()
+            );
+    }
+
+	public function testGetExtension ()
+	{
+		$link = $this->getTestLink();
+		$this->assertSame( "sqlite", $link->getExtension() );
+	}
+
+}
+
+?>
