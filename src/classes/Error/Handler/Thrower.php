fce7400c8578cbc866702f9ed4afc4f21f8a0c0c
Nycto <James@RoundEights.com>
2010-03-14 19:16:47 -0700

    Added Error\Handler\Thrower

---
+++ src/classes/Error/Handler/Thrower.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * @license Artistic License 2.0
+ *
+ * This file is part of Round Eights.
+ *
+ * Round Eights is free software: you can redistribute it and/or modify
+ * it under the terms of the Artistic License as published by
+ * the Open Source Initiative, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * Round Eights is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Artistic License for more details.
+ *
+ * You should have received a copy of the Artistic License
+ * along with Round Eights. If not, see <http://www.RoundEights.com/license.php>
+ * or <http://www.opensource.org/licenses/artistic-license-2.0.php>.
+ *
+ * @author James Frasca <James@RoundEights.com>
+ * @copyright Copyright 2009, James Frasca, All Rights Reserved
+ * @package Error
+ */
+
+namespace r8\Error\Handler;
+
+/**
+ * A Handler to throw exceptions when possible
+ */
+class Thrower implements \r8\iface\Error\Handler
+{
+
+    /**
+     * The handler being wrapped
+     *
+     * @var \r8\iface\Error\Handler
+     */
+    private $handler;
+
+    /**
+     * Constructor...
+     *
+     * @param \r8\iface\Error\Handler $handler The handler being wrapped
+     */
+    public function __construct ( \r8\iface\Error\Handler $handler = NULL )
+    {
+        $this->handler = $handler;
+    }
+
+    /**
+     * Handles an error
+     *
+     * @param \r8\iface\Error $error The error to handle
+     * @return NULL
+     */
+    public function handle ( \r8\iface\Error $error )
+    {
+        if ( $this->handler )
+            $this->handler->handle( $error );
+
+        throw new \ErrorException(
+            $error->getMessage(), $error->getCode(), $error->getCode(),
+            $error->getFile(), $error->getLine()
+        );
+    }
+
+}
+
+?>
---
+++ tests/classes/Error/Handler/Thrower.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * Unit Test File
+ *
+ * @license Artistic License 2.0
+ *
+ * This file is part of Round Eights.
+ *
+ * Round Eights is free software: you can redistribute it and/or modify
+ * it under the terms of the Artistic License as published by
+ * the Open Source Initiative, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * Round Eights is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Artistic License for more details.
+ *
+ * You should have received a copy of the Artistic License
+ * along with Round Eights. If not, see <http://www.RoundEights.com/license.php>
+ * or <http://www.opensource.org/licenses/artistic-license-2.0.php>.
+ *
+ * @author James Frasca <James@RoundEights.com>
+ * @copyright Copyright 2009, James Frasca, All Rights Reserved
+ * @package UnitTests
+ */
+
+require_once rtrim( __DIR__, "/" ) ."/../../../general.php";
+
+/**
+ * unit tests
+ */
+class classes_Error_Handler_Thrower extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Returns a test Error
+     *
+     * @return \r8\iface\Error
+     */
+    public function getTestError ()
+    {
+        $error = $this->getMock('\r8\iface\Error');
+        $error->expects( $this->once() )
+            ->method( "getMessage" )
+            ->will( $this->returnValue( "The Error Message" ) );
+        $error->expects( $this->exactly(2) )
+            ->method( "getCode" )
+            ->will( $this->returnValue( 2020 ) );
+        $error->expects( $this->once() )
+            ->method( "getFile" )
+            ->will( $this->returnValue( "/path/to/file.php" ) );
+        $error->expects( $this->once() )
+            ->method( "getLine" )
+            ->will( $this->returnValue( 11235813 ) );
+
+        return $error;
+    }
+
+    public function testHandle ()
+    {
+        $thrower = new \r8\Error\Handler\Thrower;
+
+        $error = $this->getTestError();
+
+        try {
+            $thrower->handle( $error );
+            $this->fail("An expected exception was not thrown");
+        }
+        catch ( ErrorException $err ) {
+            $this->assertEquals( "The Error Message", $err->getMessage() );
+            $this->assertEquals( 2020, $err->getCode() );
+            $this->assertEquals( 2020, $err->getSeverity() );
+            $this->assertEquals( "/path/to/file.php", $err->getFile() );
+            $this->assertEquals( 11235813, $err->getLine() );
+        }
+    }
+
+    public function testHandle_WithWrapper ()
+    {
+        $error = $this->getTestError();
+
+        $wrapped = $this->getMock('\r8\iface\Error\Handler');
+        $wrapped->expects( $this->once() )
+            ->method( "handle" )
+            ->with( $this->equalTo( $error ) );
+
+        $thrower = new \r8\Error\Handler\Thrower( $wrapped );
+
+        try {
+            $thrower->handle( $error );
+            $this->fail("An expected exception was not thrown");
+        }
+        catch ( ErrorException $err ) {
+            $this->assertEquals( "The Error Message", $err->getMessage() );
+            $this->assertEquals( 2020, $err->getCode() );
+            $this->assertEquals( 2020, $err->getSeverity() );
+            $this->assertEquals( "/path/to/file.php", $err->getFile() );
+            $this->assertEquals( 11235813, $err->getLine() );
+        }
+    }
+
+}
+
+?>
