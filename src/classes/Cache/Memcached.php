81cc7524e8b447b3b153e293a3686ebc8d973e66
Nycto <James@RoundEights.com>
2010-03-25 08:15:15 -0700

    Finished initial Cache\Memcached implementation

---
+++ src/classes/Cache/Memcached.php
@@ -0,0 +1,235 @@
+<?php
+/**
+ * @license Artistic License 2.0
+ *
+ * This file is part of Round Eights.
+ *
+ * Round Eights is free software: you can redistribute it and/or modify
+ * it under the terms of the Artistic License as published by
+ * the Open Source Initiative, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * Round Eights is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Artistic License for more details.
+ *
+ * You should have received a copy of the Artistic License
+ * along with Round Eights. If not, see <http://www.RoundEights.com/license.php>
+ * or <http://www.opensource.org/licenses/artistic-license-2.0.php>.
+ *
+ * @author James Frasca <James@RoundEights.com>
+ * @copyright Copyright 2009, James Frasca, All Rights Reserved
+ * @package Cache
+ */
+
+namespace r8\Cache;
+
+/**
+ * Adapter for the Memcached extension to the standardized cache interface
+ */
+class Memcached extends \r8\Cache\Base
+{
+
+    /**
+     * The Memcached server object
+     *
+     * @var Memcached
+     */
+    private $memcached;
+
+    /**
+     * Formats the given data for storage on the server
+     *
+     * @param Mixed $input
+     * @return String
+     */
+    static private function encode ( $value )
+    {
+        if ( $value === NULL || is_numeric($value) || is_string($value) )
+            return $value;
+
+        return serialize( $value );
+    }
+
+    /**
+     * Constructor...
+     *
+     * @param \Memcached $memcached The Memcached server object
+     */
+    public function __construct ( \Memcached $memcached )
+    {
+        if ( !extension_loaded('memcached') )
+            throw new \r8\Exception\Extension("memcached", "Extension is not loaded");
+
+        $this->memcached = $memcached;
+    }
+
+    /**
+     * Sets a new caching value, overwriting any existing values
+     *
+     * @param String $key The key for the value
+     * @param mixed $value The value to set
+     * @param Integer $expire The lifespan of this cache value, in seconds
+     * @return \r8\Cache\Memcache Returns a self reference
+     */
+    public function set ( $key, $value, $expire = 0 )
+    {
+        $this->memcached->set(
+            $key,
+            self::encode( $value ),
+            max(0, (int) $expire)
+        );
+        return $this;
+    }
+
+    /**
+     * Returns a cached value based on it's key
+     *
+     * @param String $key The value to retrieve
+     * @return mixed Returns the cached value
+     */
+    public function get ( $key )
+    {
+        $result =  $this->memcached->get( $key );
+
+        if ( $result === FALSE )
+            return NULL;
+
+        else if ( is_numeric($result) )
+            return \r8\numVal( $result );
+
+        else if ( $result == "b:0;" )
+            return FALSE;
+
+        else if ( !is_string($result) )
+            return $result;
+
+        $unserialized = @unserialize( $result );
+
+        if ( $unserialized === FALSE )
+            return $result;
+
+        return $unserialized;
+    }
+
+    /**
+     * Deletes a value from the cache
+     *
+     * @param String $key The value to delete
+     * @return \r8\Cache\Memcache Returns a self reference
+     */
+    public function delete ( $key )
+    {
+        $this->memcached->delete( $key );
+        return $this;
+    }
+
+    /**
+     * Sets a new caching value, but only if that value doesn't exist
+     *
+     * @param String $key The key for the value
+     * @param mixed $value The value to set
+     * @param Integer $expire The lifespan of this cache value, in seconds
+     * @return \r8\Cache\Memcache Returns a self reference
+     */
+    public function add ( $key, $value, $expire = 0 )
+    {
+        $this->memcached->add(
+            $key,
+            self::encode( $value ),
+            max(0, (int) $expire)
+        );
+        return $this;
+    }
+
+    /**
+     * Sets a new caching value, but only if the value already exists
+     *
+     * @param String $key The key for the value
+     * @param mixed $value The value to set
+     * @param Integer $expire The lifespan of this cache value, in seconds
+     * @return \r8\Cache\Memcache Returns a self reference
+     */
+    public function replace ( $key, $value, $expire = 0 )
+    {
+        $this->memcached->replace(
+            $key,
+            self::encode( $value ),
+            max(0, (int) $expire)
+        );
+        return $this;
+    }
+
+    /**
+     * Appends a value to the end of an existing cached value.
+     *
+     * If the value doesn't exist, it will be set with the given value
+     *
+     * @param String $key The key for the value
+     * @param mixed $value The value to append
+     * @param Integer $expire The lifespan of this cache value, in seconds
+     * @return \r8\Cache\Memcache Returns a self reference
+     */
+    public function append ( $key, $value, $expire = 0 )
+    {
+        if ( !$this->memcached->append( $key, (string) $value ) )
+            $this->set( $key, $value, $expire );
+        return $this;
+    }
+
+    /**
+     * Prepends a value to the end of an existing cached value.
+     *
+     * If the value doesn't exist, it will be set with the given value
+     *
+     * @param String $key The key for the value
+     * @param mixed $value The value to prepend
+     * @param Integer $expire The lifespan of this cache value, in seconds
+     * @return \r8\Cache\Memcache Returns a self reference
+     */
+    public function prepend ( $key, $value, $expire = 0 )
+    {
+        if ( !$this->memcached->prepend( $key, (string) $value ) )
+            $this->set( $key, $value, $expire );
+        return $this;
+    }
+
+    /**
+     * Increments a given value by one
+     *
+     * @param String $key The key for the value
+     * @return \r8\Cache\Memcache Returns a self reference
+     */
+    public function increment ( $key )
+    {
+        $this->memcached->increment( $key );
+        return $this;
+    }
+
+    /**
+     * Decrements a given value by one
+     *
+     * @param String $key The key for the value
+     * @return \r8\Cache\Memcache Returns a self reference
+     */
+    public function decrement ( $key )
+    {
+        $this->memcached->decrement( $key );
+        return $this;
+    }
+
+    /**
+     * Deletes all values in the cache
+     *
+     * @return \r8\Cache\Memcache Returns a self reference
+     */
+    public function flush ()
+    {
+        $this->memcached->flush();
+        return $this;
+    }
+
+}
+
+?>
---
+++ tests/classes/Cache/Memcached.php
@@ -0,0 +1,235 @@
+<?php
+/**
+ * Unit Test File
+ *
+ * @license Artistic License 2.0
+ *
+ * This file is part of Round Eights.
+ *
+ * Round Eights is free software: you can redistribute it and/or modify
+ * it under the terms of the Artistic License as published by
+ * the Open Source Initiative, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * Round Eights is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Artistic License for more details.
+ *
+ * You should have received a copy of the Artistic License
+ * along with Round Eights. If not, see <http://www.RoundEights.com/license.php>
+ * or <http://www.opensource.org/licenses/artistic-license-2.0.php>.
+ *
+ * @author James Frasca <James@RoundEights.com>
+ * @copyright Copyright 2009, James Frasca, All Rights Reserved
+ * @package UnitTests
+ */
+
+require_once rtrim( __DIR__, "/" ) ."/../../general.php";
+
+/**
+ * unit tests
+ */
+class classes_Cache_MemCached extends PHPUnit_Framework_TestCase
+{
+
+    public function setUp ()
+    {
+        if ( !extension_loaded('memcached') )
+            $this->markTestSkipped("Memcached extension not loaded");
+
+        // Ensure the proper configuration exists
+        $config = new r8_Test_Config( "MEMCACHE", array( "HOST", "PORT" ) );
+        $config->test();
+
+
+        $cache = new \Memcached;
+        $cache->addServer(MEMCACHE_HOST, MEMCACHE_PORT);
+        if ( $cache->getStats() === FALSE )
+            $this->markTestSkipped("Unable to connect to Memcached server");
+    }
+
+    public function getTestLink ()
+    {
+        $cache = new \Memcached;
+        $cache->addServer(MEMCACHE_HOST, MEMCACHE_PORT);
+        $cache->setOption( Memcached::OPT_COMPRESSION, FALSE );
+        return new \r8\Cache\Memcached( $cache );
+    }
+
+    public function testGet ()
+    {
+        $memcache = $this->getTestLink();
+
+        $this->assertSame(
+            $memcache,
+            $memcache->set("unitTest_key", "Chunk of Data")
+        );
+
+        $this->assertSame( "Chunk of Data", $memcache->get("unitTest_key") );
+
+
+        $this->assertSame(
+            $memcache,
+            $memcache->set("unitTest_key", "New Data")
+        );
+
+        $this->assertSame( "New Data", $memcache->get("unitTest_key") );
+    }
+
+    public function testGet_notSet ()
+    {
+        $memcache = $this->getTestLink();
+        $this->assertNull( $memcache->get("unitTest_notSet") );
+    }
+
+    public function testGet_Types ()
+    {
+        $memcache = $this->getTestLink();
+
+        $obj = new stdClass;
+        $obj->key = "Data";
+
+        $memcache->set("null", NULL);
+        $memcache->set("false", FALSE);
+        $memcache->set("true", TRUE);
+        $memcache->set("int", 12345);
+        $memcache->set("flt", 3.1415);
+        $memcache->set("str", "Data");
+        $memcache->set("ary", array(1,2,3));
+        $memcache->set("obj", $obj);
+
+        $this->assertEquals( NULL, $memcache->get("null") );
+        $this->assertEquals( FALSE, $memcache->get("false") );
+        $this->assertEquals( TRUE, $memcache->get("true") );
+        $this->assertEquals( 12345, $memcache->get("int") );
+        $this->assertEquals( 3.1415, $memcache->get("flt") );
+        $this->assertEquals( "Data", $memcache->get("str") );
+        $this->assertEquals( array(1,2,3), $memcache->get("ary") );
+        $this->assertEquals( $obj, $memcache->get("obj") );
+    }
+
+    public function testSet_Expire ()
+    {
+        $memcache = $this->getTestLink();
+
+        $this->assertSame(
+            $memcache,
+            $memcache->set("unitTest_key", "Expiring Data", 1)
+        );
+
+        usleep(1250000);
+
+        $this->assertNull( $memcache->get("unitTest_key") );
+    }
+
+    public function testDelete ()
+    {
+        $memcache = $this->getTestLink();
+
+        $memcache->set("unitTest_key", "Initial Data");
+
+        $this->assertSame( $memcache, $memcache->delete("unitTest_key") );
+
+        $this->assertNull( $memcache->get("unitTest_key") );
+    }
+
+    public function testAdd ()
+    {
+        $memcache = $this->getTestLink();
+
+        $memcache->delete("unitTest_key");
+        $this->assertSame( $memcache, $memcache->add("unitTest_key", "value") );
+        $this->assertSame("value", $memcache->get("unitTest_key"));
+
+        $this->assertSame( $memcache, $memcache->add("unitTest_key", "new") );
+        $this->assertSame("value", $memcache->get("unitTest_key"));
+    }
+
+    public function testReplace ()
+    {
+        $memcache = $this->getTestLink();
+
+        $memcache->delete("unitTest_key");
+        $this->assertSame(
+            $memcache,
+            $memcache->replace("unitTest_key", "value")
+        );
+        $this->assertNull( $memcache->get("unitTest_key") );
+
+        $memcache->set("unitTest_key", "original");
+        $this->assertSame(
+            $memcache,
+            $memcache->replace("unitTest_key", "value")
+        );
+        $this->assertSame("value", $memcache->get("unitTest_key"));
+    }
+
+    public function testIncrement ()
+    {
+        $memcache = $this->getTestLink();
+        $memcache->set("unitTest_key", 1);
+
+        $this->assertSame($memcache, $memcache->increment("unitTest_key"));
+        $this->assertSame(2, $memcache->get("unitTest_key"));
+
+        $this->assertSame($memcache, $memcache->increment("unitTest_key"));
+        $this->assertSame(3, $memcache->get("unitTest_key"));
+    }
+
+    public function testDecrement ()
+    {
+        $memcache = $this->getTestLink();
+        $memcache->set("unitTest_key", 3);
+
+        $this->assertSame($memcache, $memcache->decrement("unitTest_key"));
+        $this->assertSame(2, $memcache->get("unitTest_key"));
+
+        $this->assertSame($memcache, $memcache->decrement("unitTest_key"));
+        $this->assertSame(1, $memcache->get("unitTest_key"));
+    }
+
+    public function testAppend ()
+    {
+        $memcache = $this->getTestLink();
+        $memcache->delete("unitTest_key");
+
+        $memcache->append("unitTest_key", "first");
+        $this->assertSame("first", $memcache->get("unitTest_key"));
+
+        $memcache->append("unitTest_key", "second");
+        $this->assertSame("firstsecond", $memcache->get("unitTest_key"));
+
+        $memcache->append("unitTest_key", "third");
+        $this->assertSame("firstsecondthird", $memcache->get("unitTest_key"));
+    }
+
+    public function testPrepend ()
+    {
+        $memcache = $this->getTestLink();
+        $memcache->delete("unitTest_key");
+
+        $memcache->prepend("unitTest_key", "first");
+        $this->assertSame("first", $memcache->get("unitTest_key"));
+
+        $memcache->prepend("unitTest_key", "second");
+        $this->assertSame("secondfirst", $memcache->get("unitTest_key"));
+
+        $memcache->prepend("unitTest_key", "third");
+        $this->assertSame("thirdsecondfirst", $memcache->get("unitTest_key"));
+    }
+
+    public function testFlush ()
+    {
+        $memcache = $this->getTestLink();
+
+        $memcache->set("unitTest_key", "Value");
+
+        $this->assertSame( $memcache, $memcache->flush() );
+
+        $this->assertNull( $memcache->get("unitTest_key") );
+    }
+
+}
+
+?>
