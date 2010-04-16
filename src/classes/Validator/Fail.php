6cf08c7a8d40a40522b62bed1ac654e4ec529905
Nycto <James@RoundEights.com>
2010-04-16 08:21:10 -0700

    Added Validator\Fail

---
+++ src/classes/Validator/Fail.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * @license Artistic License 2.0
+ *
+ * This file is part of Round Eights.
+ *
+ * Round Eights is free software: you can redistribute it and/or modify
+ * it under the terms of the Artistic License as published by
+ * the Open Source Initiative, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * Round Eights is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Artistic License for more details.
+ *
+ * You should have received a copy of the Artistic License
+ * along with Round Eights. If not, see <http://www.RoundEights.com/license.php>
+ * or <http://www.opensource.org/licenses/artistic-license-2.0.php>.
+ *
+ * @author James Frasca <James@RoundEights.com>
+ * @copyright Copyright 2009, James Frasca, All Rights Reserved
+ * @package Validators
+ */
+
+namespace r8\Validator;
+
+/**
+ * A validator that will always return an error
+ */
+class Fail extends \r8\Validator
+{
+
+    /**
+     * The error message to return
+     *
+     * @var String
+     */
+    private $message;
+
+    /**
+     * Constructor...
+     *
+     * @param String $message The error message to return
+     */
+    public function __construct( $message )
+    {
+        $this->message = (string) $message;
+    }
+
+    /**
+     * Validates the given value
+     *
+     * @param mixed $value The value to validate
+     * @return String|NULL Any errors encountered
+     */
+    protected function process ( $value )
+    {
+        return $this->message;
+    }
+
+}
+
+?>
