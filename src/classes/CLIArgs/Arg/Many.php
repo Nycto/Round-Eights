37e6aedd8b10f71440e93c499eb06391038fe154
Nycto <James@RoundEights.com>
2010-06-20 15:29:57 -0700

    Added CLIArgs\Arg\Many

---
+++ src/classes/CLIArgs/Arg/Many.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * @license Artistic License 2.0
+ *
+ * This file is part of Round Eights.
+ *
+ * Round Eights is free software: you can redistribute it and/or modify
+ * it under the terms of the Artistic License as published by
+ * the Open Source Initiative, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * Round Eights is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Artistic License for more details.
+ *
+ * You should have received a copy of the Artistic License
+ * along with Round Eights. If not, see <http://www.RoundEights.com/license.php>
+ * or <http://www.opensource.org/licenses/artistic-license-2.0.php>.
+ *
+ * @author James Frasca <James@RoundEights.com>
+ * @copyright Copyright 2009, James Frasca, All Rights Reserved
+ * @package CLIArgs
+ */
+
+namespace r8\CLIArgs\Arg;
+
+/**
+ * A command line argument that consumes a single argument
+ */
+class Many extends \r8\CLIArgs\Arg
+{
+
+    /**
+     * Given an input of arguments, pops elements off, filters them, validates
+     * them and returns an array of everything it consumes
+     *
+     * @param \r8\CLIArgs\Input $input The input list to consume
+     * @return Array
+     */
+    public function consume ( \r8\CLIArgs\Input $input )
+    {
+        $result = array();
+
+        while ( $input->hasNextArg() )
+        {
+            $arg = $this->getFilter()->filter( $input->popArgument() );
+            $this->getValidator()->ensure( $arg );
+            $result[] = $arg;
+        }
+
+        return $result;
+    }
+
+}
+
+?>
---
+++ tests/classes/CLIArgs/Arg/Many.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Unit Test File
+ *
+ * @license Artistic License 2.0
+ *
+ * This file is part of Round Eights.
+ *
+ * Round Eights is free software: you can redistribute it and/or modify
+ * it under the terms of the Artistic License as published by
+ * the Open Source Initiative, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * Round Eights is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Artistic License for more details.
+ *
+ * You should have received a copy of the Artistic License
+ * along with Round Eights. If not, see <http://www.RoundEights.com/license.php>
+ * or <http://www.opensource.org/licenses/artistic-license-2.0.php>.
+ *
+ * @author James Frasca <James@RoundEights.com>
+ * @copyright Copyright 2009, James Frasca, All Rights Reserved
+ * @package UnitTests
+ */
+
+require_once rtrim( __DIR__, "/" ) ."/../../../general.php";
+
+/**
+ * Unit Tests
+ */
+class classes_CLIArgs_Arg_Many extends PHPUnit_Framework_TestCase
+{
+
+    public function testConsume_Empty ()
+    {
+        $arg = new \r8\CLIArgs\Arg\Many(
+            "test",
+            new \r8\Curry\Call('strtoupper'),
+            new \r8\Validator\NotEmpty
+        );
+
+        $this->assertSame(
+            array(),
+            $arg->consume(
+                new \r8\CLIArgs\Input( array() )
+            )
+        );
+    }
+
+    public function testConsume_Valid ()
+    {
+        $arg = new \r8\CLIArgs\Arg\Many(
+            "test",
+            new \r8\Curry\Call('strtoupper'),
+            new \r8\Validator\NotEmpty
+        );
+
+        $this->assertSame(
+            array("ONE", "TWO", "THREE"),
+            $arg->consume(
+                new \r8\CLIArgs\Input( array("one", "two", "three") )
+            )
+        );
+    }
+
+    public function testConsume_Invalid ()
+    {
+        $arg = new \r8\CLIArgs\Arg\Many(
+            "test",
+            new \r8\Curry\Call('strtoupper'),
+            new \r8\Validator\Compare("=", "one")
+        );
+
+        try {
+            $arg->consume( new \r8\CLIArgs\Input( array("one", "two") ) );
+            $this->fail("An expected exception was not thrown");
+        }
+        catch ( \r8\Exception\Data $err ) {}
+    }
+
+}
+
+?>
