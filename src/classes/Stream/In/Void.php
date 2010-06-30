fdfbd3f4714490f8f54dd38c582d007399ee83e5
Nycto <James@RoundEights.com>
2010-06-29 20:47:12 -0700

    Added Stream\In\Void

---
+++ src/classes/Stream/In/Void.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * @license Artistic License 2.0
+ *
+ * This file is part of Round Eights.
+ *
+ * Round Eights is free software: you can redistribute it and/or modify
+ * it under the terms of the Artistic License as published by
+ * the Open Source Initiative, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * Round Eights is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Artistic License for more details.
+ *
+ * You should have received a copy of the Artistic License
+ * along with Round Eights. If not, see <http://www.RoundEights.com/license.php>
+ * or <http://www.opensource.org/licenses/artistic-license-2.0.php>.
+ *
+ * @author James Frasca <James@RoundEights.com>
+ * @copyright Copyright 2009, James Frasca, All Rights Reserved
+ * @package Stream
+ */
+
+namespace r8\Stream\In;
+
+/**
+ * Provides a Stream interface that will always return an empty value
+ */
+class Void implements \r8\iface\Stream\In
+{
+
+    /**
+     * Returns whether there is any more information to read from this stream
+     *
+     * @return Boolean
+     */
+    public function canRead ()
+    {
+        return FALSE;
+    }
+
+    /**
+     * Reads a given number of bytes from this stream
+     *
+     * @param Integer $bytes The number of bytes to read
+     * @return String|NULL Null is returned if there is no data available to read
+     */
+    public function read ( $bytes )
+    {
+        return NULL;
+    }
+
+    /**
+     * Reads the remaining data from this stream
+     *
+     * @return String
+     */
+    public function readAll ()
+    {
+        return NULL;
+    }
+
+    /**
+     * Rewinds this stream back to the beginning
+     *
+     * @return \r8\iface\Stream\In Returns a self reference
+     */
+    public function rewind ()
+    {
+        return $this;
+    }
+
+}
+
+?>
