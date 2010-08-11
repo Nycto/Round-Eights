2854357339b155b645f343e3598b75a75415fcce
Nycto <James@RoundEights.com>
2010-08-11 08:23:44 -0700

    Added Log\Node\Void

---
+++ src/classes/Log/Node/Void.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @license Artistic License 2.0
+ *
+ * This file is part of Round Eights.
+ *
+ * Round Eights is free software: you can redistribute it and/or modify
+ * it under the terms of the Artistic License as published by
+ * the Open Source Initiative, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * Round Eights is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Artistic License for more details.
+ *
+ * You should have received a copy of the Artistic License
+ * along with Round Eights. If not, see <http://www.RoundEights.com/license.php>
+ * or <http://www.opensource.org/licenses/artistic-license-2.0.php>.
+ *
+ * @author James Frasca <James@RoundEights.com>
+ * @copyright Copyright 2009, James Frasca, All Rights Reserved
+ * @package Log
+ */
+
+namespace r8\Log\Node;
+
+/**
+ * An empty log node that simply drops all the messages dispatched to it
+ */
+class Void implements \r8\iface\Log\Node
+{
+
+    /**
+     * @see \r8\iface\Log\Node::dispatch
+     */
+    public function dispatch ( \r8\Log\Message $message )
+    {
+        return $this;
+    }
+
+}
+
+?>
