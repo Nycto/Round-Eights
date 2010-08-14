a8cb287d7a2efa5de4f1ba11947277f547ce8bc9
Nycto <James@RoundEights.com>
2010-08-13 19:42:33 -0700

    Added Log\Matcher\Code

---
+++ src/classes/Log/Matcher/Code.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * @license Artistic License 2.0
+ *
+ * This file is part of Round Eights.
+ *
+ * Round Eights is free software: you can redistribute it and/or modify
+ * it under the terms of the Artistic License as published by
+ * the Open Source Initiative, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * Round Eights is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Artistic License for more details.
+ *
+ * You should have received a copy of the Artistic License
+ * along with Round Eights. If not, see <http://www.RoundEights.com/license.php>
+ * or <http://www.opensource.org/licenses/artistic-license-2.0.php>.
+ *
+ * @author James Frasca <James@RoundEights.com>
+ * @copyright Copyright 2009, James Frasca, All Rights Reserved
+ * @package Log
+ */
+
+namespace r8\Log\Matcher;
+
+/**
+ * Uses a validator to match the code of a log message
+ */
+class Code implements \r8\iface\Log\Matcher
+{
+
+    /**
+     * The validator to match against
+     *
+     * @var \r8\iface\Validator
+     */
+    private $validator;
+
+    /**
+     * Constructor...
+     *
+     * @param \r8\iface\Validator $validator
+     */
+    public function __construct ( \r8\iface\Validator $validator )
+    {
+        $this->validator = $validator;
+    }
+
+    /**
+     * @see \r8\iface\Log\Matcher::matches
+     */
+    public function matches ( \r8\Log\Message $message )
+    {
+        return $this->validator->isValid( $message->getCode() );
+    }
+
+}
+
+?>
